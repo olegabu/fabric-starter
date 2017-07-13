63609e4207cc48e2c9b1290f43b42f0c608fbf7e
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-07-13 23:38:10 +0300

    +test folder

--- api_server/lib/express-promise.js
+++ api_server/lib/express-promise.js
@@ -3,48 +3,49 @@
  */
 "use strict";
 
-module.exports = function(){
-    return expressPromise;
-};
+/**
+ * @param {function(err:Error, req:Request, res:Response)} [errorReporter]
+ * @returns {expressPromise}
+ */
+module.exports = function(errorReporter){
+    errorReporter = errorReporter || reportErrorDefault;
+    return function expressPromise(req, res, next){
 
-function expressPromise(req,res,next){
-
-    /*
-     Report error
-     You can use it in severqal ways:
-     - promise.catch() from promise
-     - res.error(new Error())  - pass any error object
-     - res.error({status:401, message:'alala'})  - pass any object
-     - res.error(401, 'alala')  - pass code and message separately
-     - res.error('alala')  - pass only message.
-     */
-    res.error = (err, msg)=>{
-        if(err === 401){
+        /*
+           Report error
+           You can use it in several ways:
+           - promise.catch() from promise
+           - res.error(new Error())  - pass any error object
+           - res.error({status:401, message:'alala'})  - pass any object
+           - res.error(401, 'alala')  - pass code and message separately
+           - res.error('alala')  - pass only message.
+           */
+        res.error = (err, msg)=>{
+          if(err === 401){
             err = {status:401, message: msg || 'Unauthorized'};
-        }else if(typeof err === 'number' && typeof msg === 'string'){
+          }else if(typeof err === 'number' && typeof msg === 'string'){
             err = {status:err, message: msg};
-        }
-        report_error(res, err);
+          }
+          errorReporter(err, req, res);
+        };
+
+        // add res.promise(<promise>) to easy handle promises
+        res.promise = (promise)=>{
+          return promise.then(data=>res.send(data)).catch(res.error);
+        };
+        next();
     };
-
-    // add res.promise(<promise>) to easy handle promises
-    res.promise = (promise)=>{
-        return promise.then(data=>res.send(data)).catch(res.error);
-    };
-    next();
-}
-
-
+};
 
 /**
  *
  */
-function report_error(res, err){
+function reportErrorDefault(err, req, res){
     let status = err.status || err.statusCode || 500;
     let message = err.message || err.description || err || 'Unknown error';
     res.status(status).send({ok:false, message:message});
 
-    console.error('Request failed: %s %s:', res.req.method.toUpperCase(), res.req.url, message );
+    console.error('Request failed: %s %s:', req.method.toUpperCase(), req.url, message );
     if(err.stack){
         console.error(err.stack);
     }
---
+++ api_server/test/lib/express-promise.test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by maksim on 7/13/17.
+ */
+var expressPromise = require('../../lib/express-promise');
+
+describe('express-promise', function(){
+
+  it('test', function(){
+
+
+  });
+
+});
