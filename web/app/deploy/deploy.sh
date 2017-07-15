372abd1258766b4102b1e937358f65dd73959b2b
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-07-15 16:11:34 +0300

    remove unused files

--- web/app/deploy/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -XPOST -d @deployRemote.json http://vp1:7050/chaincode
-
-
-curl -XPOST -d  '{"jsonrpc": "2.0", "method": "deploy",  "params": {"type": 1,"chaincodeID": {"path": "github.com/olegabu/catbond/chaincode","language": "GOLANG"}, "ctorMsg": { "args": ["aW5pdA=="] },"secureContext": "issuer0", "attributes": ["role"]},"id": 0}' http://vp1:7050/chaincode
