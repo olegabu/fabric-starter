3a6a14622c8f077446a9c30247c7a71c4c4de0f9
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-07-28 18:22:52 +0300

    fix #1 path

--- server/lib-fabric/hfc.js
+++ server/lib-fabric/hfc.js
@@ -1,21 +1,22 @@
 /*
 
  */
+const RELPATH = '/../'; // relative path to server root. Change it during file movement
 const path = require('path');
+const fs   = require('fs');
 
 var log4js = require('log4js');
 var logger = log4js.getLogger('fabric-client');
 logger.setLevel('DEBUG');
 
-const CONFIG_DEFAULT = '../../artifacts/network-config.json';
-// const CRYPTO_CONFIG_DIR_DEFAULT = '../../artifacts/crypto-config';
+const CONFIG_DEFAULT = '../artifacts/network-config.json';
+// const CRYPTO_CONFIG_DIR_DEFAULT = '../artifacts/crypto-config';
 
 ////
 var config = process.env.CONFIG_FILE || CONFIG_DEFAULT;
 if(!path.isAbsolute(config)){
-  config = path.join(__dirname,'/../', config);
+  config = path.join(__dirname, RELPATH, config);
 }
-logger.info('Use network config file: %s', config);
 
 // TODO: use basepath for 'crypto-config' folder in network-config.json
 // var cryptoConfigDir = process.env.CRYPTO_CONFIG_DIR || CRYPTO_CONFIG_DIR_DEFAULT;
@@ -26,7 +27,8 @@ logger.info('Use network config file: %s', config);
 
 
 
-
+logger.info('Use network config file: %s', config);
+fs.accessSync(config, fs.constants.R_OK);
 
 ///////
 var hfc = require('fabric-client');
