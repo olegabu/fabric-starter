8fd03e02367a404c0d1a5b06aebe81da0c2a0d87
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-07-28 18:33:37 +0300

    remove unnecessary file

--- docker-tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker tag hyperledger/fabric-tools:x86_64-1.0.0  hyperledger/fabric-tools:latest
-docker tag hyperledger/fabric-orderer:x86_64-1.0.0  hyperledger/fabric-orderer:latest
-docker tag hyperledger/fabric-peer:x86_64-1.0.0  hyperledger/fabric-peer:latest
-docker tag hyperledger/fabric-ca:x86_64-1.0.0  hyperledger/fabric-ca:latest
