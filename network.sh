62751f8268aef379478d9875c90d35d63a79b9f1
Maksim Chartkou <maksim.chartkou@altoros.com>
2017-07-31 16:12:42 +0300

    re-use certificates in dev mode

--- network.sh
+++ network.sh
@@ -26,9 +26,6 @@ function removeArtifacts() {
   echo "Removing generated artifacts"
   rm -rf artifacts/crypto-config
   rm -rf artifacts/channel
-  # remove dev
-  rm -rf artifacts-dev/crypto-config
-  rm -rf artifacts-dev/channel
 }
 
 function removeDockers() {
@@ -74,10 +71,6 @@ function generateArtifacts() {
 
     # docker generates files to mapped volumes as root, change ownership
     chown -R 1000:1000 artifacts/
-
-    #copy to dev
-    cp -R artifacts/channel       artifacts-dev/channel
-    cp -R artifacts/crypto-config artifacts-dev/crypto-config
 }
 
 function networkUp () {
