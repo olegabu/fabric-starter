1e964d879672dfbbf514ef3484289bc1d49ab1e3
LeonidLe <leonid.leshchenya@altoros.com>
2018-03-28 16:42:46 +0300

    deployment adjusts for latest components versions

--- init-fabric.sh
+++ init-fabric.sh
@@ -15,22 +15,34 @@ FABRIC_PACK="x86_64-${FABRIC_VERSION}"
 
 sudo apt-get update && sudo apt-get -y install docker-compose jq
 
+echo "Remove old images"
 sudo docker rmi -f $(docker images -q)
 
+echo "Pull hyperledger/fabric-ca"
 sudo docker pull hyperledger/fabric-ca:${FABRIC_PACK}
+echo "Pull hyperledger/fabric-orderer"
 sudo docker pull hyperledger/fabric-orderer:${FABRIC_PACK}
+echo "Pull hyperledger/fabric-peer"
 sudo docker pull hyperledger/fabric-peer:${FABRIC_PACK}
+echo "Pull hyperledger/fabric-ccenv"
 sudo docker pull hyperledger/fabric-ccenv:${FABRIC_PACK}
 #docker pull hyperledger/fabric-buildenv:${FABRIC_PACK}
 
+echo "Pull hyperledger/fabric-tools"
 sudo docker pull hyperledger/fabric-tools:${FABRIC_PACK}
 #workaround until fixed in 1.1.0-alpha
+echo "Pull hyperledger/fabric-tools"
 sudo docker pull hyperledger/fabric-tools:x86_64-1.1.0-preview
 
+echo "Pull hyperledger/fabric-baseos"
 sudo docker pull hyperledger/fabric-baseos:x86_64-0.4.5
+echo "Pull hyperledger/fabric-baseos"
 sudo docker pull hyperledger/fabric-baseos:x86_64-0.4.6
+echo "Pull hyperledger/fabric-rest"
 sudo docker pull maxxx1313/fabric-rest
+echo "Pull nginx"
 sudo docker pull nginx
+echo "Pull node:6-alpine"
 sudo docker pull node:6-alpine
 export FABRIC_PACK
 
