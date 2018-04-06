5179c77c367804296fdf5326238c1ee28de36b66
LeonidLe <leonid.leshchenya@altoros.com>
2018-04-06 14:25:45 +0300

    remove unnecessary 1.1.0-preview

--- init-fabric.sh
+++ init-fabric.sh
@@ -30,12 +30,7 @@ sudo docker pull hyperledger/fabric-ccenv:${FABRIC_PACK}
 
 echo "Pull hyperledger/fabric-tools"
 sudo docker pull hyperledger/fabric-tools:${FABRIC_PACK}
-#workaround until fixed in 1.1.0-alpha
-echo "Pull hyperledger/fabric-tools"
-sudo docker pull hyperledger/fabric-tools:x86_64-1.1.0-preview
 
-echo "Pull hyperledger/fabric-baseos"
-sudo docker pull hyperledger/fabric-baseos:x86_64-0.4.5
 echo "Pull hyperledger/fabric-baseos"
 sudo docker pull hyperledger/fabric-baseos:x86_64-0.4.6
 echo "Pull hyperledger/fabric-rest"
