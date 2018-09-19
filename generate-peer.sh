07d1d2dfd7832ab227a597fccc69e26ceb130b3a
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2018-09-19 19:17:49 +0300

    rename Admin's private key to sk.pem

--- generate-peer.sh
+++ generate-peer.sh
@@ -5,4 +5,5 @@ runCLI "rm -rf crypto-config/peerOrganizations/$ORG.$DOMAIN \
     && envsubst <templates/cryptogen-peer-template.yaml >crypto-config/cryptogen-$ORG.yaml \
     && cryptogen generate --config=crypto-config/cryptogen-$ORG.yaml \
     && mv crypto-config/peerOrganizations/$ORG.$DOMAIN/ca/*_sk crypto-config/peerOrganizations/$ORG.$DOMAIN/ca/sk.pem \
+    && mv crypto-config/peerOrganizations/$ORG.$DOMAIN/users/Admin@$ORG.$DOMAIN/msp/keystore/*_sk crypto-config/peerOrganizations/$ORG.$DOMAIN/users/Admin@$ORG.$DOMAIN/msp/keystore/sk.pem \
     && chown $UID -R crypto-config/"
