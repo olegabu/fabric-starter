a7fd3c568f387bdd3adc05762e7b502d60d5bafa
Andrew Pudovikov <pudovikov@me.com>
2018-10-04 20:40:08 +0300

    chaincode query&invoke args fix peer ENV vars in yaml config

--- chaincode-query.sh
+++ chaincode-query.sh
@@ -4,8 +4,8 @@ usageMsg="$0 chaincodeName channelName [args='[]']"
 exampleMsg="$0 chaincode1 common '[\"query\",\"a\"]' "
 
 IFS=
-chaincodeName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
-channelName=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
+channelName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
+chaincodeName=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
 arguments=${3-'[]'}
 
 queryChaincode "$channelName" "$chaincodeName" "$arguments"
