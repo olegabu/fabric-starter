720fb767b5bdb039a84ee83de507f6e6a3a26cdb
Andrew Pudovikov <pudovikov@me.com>
2018-10-04 16:56:54 +0300

    fix for generate-peer script - case without using docker machine

--- generate-peer.sh
+++ generate-peer.sh
@@ -3,7 +3,7 @@ source lib.sh
 
 ## Workaround until docker-compose issue 4601 is solved
 # https://github.com/docker/compose/issues/4601
-docker run -dit --name alpine --network fabric-overlay alpine
+[ -n "${DOCKER_HOST}" ] && docker run -dit --name alpine --network fabric-overlay alpine
 
 envSubst "templates/cryptogen-peer-template.yaml" "crypto-config/cryptogen-$ORG.yaml"
 runCLI "rm -rf crypto-config/peerOrganizations/$ORG.$DOMAIN \
