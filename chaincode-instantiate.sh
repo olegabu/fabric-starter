546ec18d1c5d113c2dff359dfd2c95e6ca8cf6d9
Andrew Pudovikov <pudovikov@me.com>
2018-10-05 22:10:44 +0300

    chaincode invoke fix and other

--- chaincode-instantiate.sh
+++ chaincode-instantiate.sh
@@ -4,12 +4,11 @@ usageMsg="$0 chaincodeName channelName [init args='[]'] [version=1.0]"
 exampleMsg="$0 chaincode1 common '[\"Init\",\"a\",\"10\", \"b\", \"0\"]'"
 
 IFS=
-chaincodeName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
-channelName=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
+channelName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
+chaincodeName=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
 initArguments=${3-'[]'}
 chaincodeVersion=${4-1.0}
-privateCollection=${5}
-privateCollectionPath=${6}
+privateCollectionPath=${5}
 
 
-instantiateChaincode "$channelName" "$chaincodeName" "$initArguments" "$chaincodeVersion" "$privateCollection" "$privateCollectionPath"
+instantiateChaincode "$channelName" "$chaincodeName" "$initArguments" "$chaincodeVersion" "$privateCollectionPath"
