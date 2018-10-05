22bee80c3957f91cb6ad8fd2313bcf62a88ead97
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2018-10-05 18:22:19 +0300

    machine-create-peer.sh tries to get token of not specified

--- machine-create-peer.sh
+++ machine-create-peer.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 source lib.sh
-usageMsg="$0 swarmToken swarmManagerIp [org=org1] [domain=example.com]"
-exampleMsg="$0 SWMTKN-1-4fbasgnyfz5uqhybbesr9gbhg0lqlcj5luotclhij87owzd4ve-4k16civlmj3hfz1q715csr8lf 192.168.99.102 org3"
+usageMsg="$0 org=org1 [swarmToken] [swarmManagerIp] [domain=example.com]"
+exampleMsg="$0 org3 SWMTKN-1-4fbasgnyfz5uqhybbesr9gbhg0lqlcj5luotclhij87owzd4ve-4k16civlmj3hfz1q715csr8lf 192.168.99.100"
 
 IFS=
-swarmToken=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
-swarmManagerIp=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
-org=${3-org1}
+org=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
+swarmToken=${2}
+swarmManagerIp=${3}
 domain=${4-example.com}
 
 export ORG=${org}
 export DOMAIN=${domain}
 
-printInColor "1;32" "Creating machine $ORG in $DOMAIN"
+printInColor "1;33" "Creating machine $ORG in $DOMAIN"
+
+if [ -z "$swarmToken" ]; then
+    printInColor "1;35" "get swarm token and swarm manager ip address from 'orderer' machine"
+    eval $(docker-machine env orderer)
+    swarmToken=`docker swarm join-token worker -q`
+    swarmManagerIp=`docker-machine ip orderer`
+    [ $? -ne 0 ] && printRedYellow "'orderer' machine is not available. Create 'orderer' machine first or specify swarmToken and swarmManagerIp to join to." && exit 1
+fi
+
 
 export ORGS="{\"$ORG\":\"peer0.$ORG.$DOMAIN:7051\"}" CAS="{\"$ORG\":\"ca.$ORG.$DOMAIN:7054\"}"
 docker-machine create --driver virtualbox ${ORG}
 eval "$(docker-machine env ${ORG})"
+echo; env | grep DOCKER; echo
 docker swarm join --token ${swarmToken} ${swarmManagerIp}:2377
 docker run -dit --name alpine --network fabric-overlay alpine
 docker-machine scp -r templates ${ORG}:templates
