d648352510ebf44c93c8d1eee7205245c5b1f80a
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2018-10-12 11:48:37 +0300

    clean not work with runCLI

--- clean.sh
+++ clean.sh
@@ -3,14 +3,17 @@ source lib.sh
 
 docker rm -f $(docker ps -aq)
 #TODO [ "${DOCKER_MACHINE_NAME}" == "orderer" ]  && EXECUTE_BY_ORDERER=1 runCLIWithComposerOverrides down || runCLIWithComposerOverrides down
-if [ -z "$DOCKER_HOST" ] ; then
-    runCLI "rm -rf crypto-config/"
-    runCLI "rm -rf data/"
-else
-    docker-machine ssh ${DOCKER_MACHINE_NAME} sudo rm -rf crypto-config
-fi
 
 docker volume prune -f
 docker rmi -f $(docker images -q -f "reference=dev-*")
+
+if [ -z "$DOCKER_HOST" ] ; then
+     sudo rm -rf crypto-config/
+     sudo rm -rf data/
+else
+    docker-machine ssh ${DOCKER_MACHINE_NAME} sudo rm -rf crypto-config
+    docker-machine ssh ${DOCKER_MACHINE_NAME} sudo rm -rf data/
+fi
+
 #docker rmi -f $(docker images -q -f "reference=olegabu/fabric-starter-client")
 #docker network rm `(docker network ls -q)`
