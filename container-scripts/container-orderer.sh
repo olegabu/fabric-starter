9222eb442c015fe21dd260eb1eaf563bad2ff1d1
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-03-07 19:22:45 +0300

    temporal fix for #48

--- container-scripts/container-orderer.sh
+++ container-scripts/container-orderer.sh
@@ -9,6 +9,10 @@ if [ ! -f "crypto-config/ordererOrganizations/$DOMAIN/orderers/orderer.$DOMAIN/m
     envsubst < "templates/cryptogen-orderer-template.yaml" > "crypto-config/cryptogen-orderer.yaml"
     rm -rf crypto-config/ordererOrganizations
     cryptogen generate --config=crypto-config/cryptogen-orderer.yaml
+
+    mkdir -p crypto-config/ordererOrganizations/$DOMAIN/msp/well-known
+    cp crypto-config/ordererOrganizations/$DOMAIN/msp/tlscacerts/tlsca.$DOMAIN-cert.pem crypto-config/ordererOrganizations/$DOMAIN/msp/well-known/msp-admin.pem 2>/dev/null
+    cp crypto-config/ordererOrganizations/$DOMAIN/msp/tlscacerts/tlsca.$DOMAIN-cert.pem crypto-config/ordererOrganizations/$DOMAIN/msp/well-known/tlsca-cert.pem 2>/dev/null
 else
     echo "Orderer MSP exists. Generation skipped".
 fi
--- container-scripts/container-peer.sh
+++ container-scripts/container-peer.sh
@@ -24,6 +24,10 @@ if [ ! -d "crypto-config/peerOrganizations/$ORG.$DOMAIN/peers/peer0.$ORG.$DOMAIN
     mv crypto-config/peerOrganizations/$ORG.$DOMAIN/ca/*_sk crypto-config/peerOrganizations/$ORG.$DOMAIN/ca/sk.pem
     mv crypto-config/peerOrganizations/$ORG.$DOMAIN/users/Admin@$ORG.$DOMAIN/msp/keystore/*_sk crypto-config/peerOrganizations/$ORG.$DOMAIN/users/Admin@$ORG.$DOMAIN/msp/keystore/sk.pem
     cp -r crypto-config/ordererOrganizations/$DOMAIN/msp/* crypto-config/peerOrganizations/$ORG.$DOMAIN/msp 2>/dev/null
+
+    mkdir -p crypto-config/peerOrganizations/$ORG.$DOMAIN/msp/well-known
+    cp crypto-config/peerOrganizations/$ORG.$DOMAIN/msp/tlscacerts/tlsca.$ORG.$DOMAIN-cert.pem crypto-config/peerOrganizations/$ORG.$DOMAIN/msp/well-known/msp-admin.pem 2>/dev/null
+    cp crypto-config/peerOrganizations/$ORG.$DOMAIN/msp/tlscacerts/tlsca.$ORG.$DOMAIN-cert.pem crypto-config/peerOrganizations/$ORG.$DOMAIN/msp/well-known/tlsca-cert.pem 2>/dev/null
 else
     echo "$ORG MSP exists. Generation skipped".
 fi
