c21550028a8482969fd166eba3e2eac9cbde2d94
Oleg Abdrashitov <olegabu73@gmail.com>
2019-03-11 19:16:01 +0300

    moved create multihost nodes functions into lib to be shared between scripts

---
+++ network-add-org.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+source lib.sh
+
+export MULTIHOST=true
+export DOMAIN=${DOMAIN-example.com}
+
+: ${CHANNEL:=common}
+: ${CHAINCODE_INSTALL_ARGS:=reference}
+: ${CHAINCODE_INSTANTIATE_ARGS:=common reference}
+: ${DOCKER_COMPOSE_ARGS:= -f docker-compose.yaml -f docker-compose-couchdb.yaml -f multihost.yaml -f docker-compose-ports.yaml}
+: ${CHAINCODE_HOME:=chaincode}
+: ${WEBAPP_HOME:=webapp}
+: ${MIDDLEWARE_HOME:=middleware}
+
+# Set WORK_DIR as home dir on remote machine
+setMachineWorkDir
+
+org=${1}
+
+copyFileToMachine ${org} hosts hosts
+
+copyDirToMachine ${org} templates ${WORK_DIR}/templates
+copyDirToMachine ${org} ${CHAINCODE_HOME} ${WORK_DIR}/${CHAINCODE_HOME}
+copyDirToMachine ${org} ${WEBAPP_HOME} ${WORK_DIR}/${WEBAPP_HOME}
+copyDirToMachine ${org} ${MIDDLEWARE_HOME} ${WORK_DIR}/${MIDDLEWARE_HOME}
+
+info "Copying dns chaincode to remote machine ${machine}"
+machine="$org.$DOMAIN"
+docker-machine ssh ${machine} mkdir -p ${WORK_DIR}/chaincode/node
+docker-machine scp -r chaincode/node/dns ${machine}:${WORK_DIR}/chaincode/node
+
+connectMachine ${org}
+
+./clean.sh
+docker-compose ${DOCKER_COMPOSE_ARGS} up -d
+
+# Install application and dns chaincodes
+
+./chaincode-install.sh ${CHAINCODE_INSTALL_ARGS}
+#./chaincode-install.sh dns
+
+## Add member organizations to the consortium
+#
+#connectMachine orderer
+#
+#info "Adding $org to the consortium"
+#./consortium-add-org.sh ${org}
+
