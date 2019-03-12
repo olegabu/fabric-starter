ececa60c0657489f005aaf1ddd1fa4cb4f61d6d4
Oleg Abdrashitov <olegabu73@gmail.com>
2019-03-12 12:35:52 +0300

    Query reference chaincode

--- smoke-test.sh
+++ smoke-test.sh
@@ -6,11 +6,14 @@ export MULTIHOST=true
 export DOMAIN=${DOMAIN-example.com}
 
 first_org=${1:-org1}
-
-info "Smoke test queries dns chaincode via rest api"
-
 ip=$(getMachineIp ${first_org})
-jwt=`(curl -d '{"username":"user1","password":"pass"}' -H "Content-Type: application/json" http://${ip}:4000/users | tr -d '"')`
-curl -H "Authorization: Bearer $jwt" "http://$ip:4000/channels/common/chaincodes/dns?fcn=range&unescape=true"
 
+info "Smoke test logs into $first_org at $ip and queries dns chaincode via rest api"
+
+jwt=`(curl -d '{"username":"user1","password":"pass"}' -H "Content-Type: application/json" http://${ip}:4000/users | tr -d '"')`
+curl -H "Authorization: Bearer $jwt" "http://$ip:4000/channels/common/chaincodes/dns?fcn=range"
+echo
+
+info "Query reference chaincode"
+curl -H "Authorization: Bearer $jwt" "http://$ip:4000/channels/common/chaincodes/reference?fcn=range"
 echo
