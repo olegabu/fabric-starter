53612121c57c644dd22c5acf59f5d3f576b4a486
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-05-22 15:18:05 +0300

    jenkins

--- pipelines/CreateStable_Jenkinsfile.groovy
+++ pipelines/CreateStable_Jenkinsfile.groovy
@@ -1,57 +1,87 @@
-#! /usr/bin/env groovy
+#!/usr/bin/env groovy
+
+// Parameters:
+// DOCKER_REGISTRY: https://registry-1.docker.io/v2
+// DOCKER_REPO: olegabu
+// DOCKER_CREDENTIALS_ID: dockerhub
+//
+// GIT_REPO_OWNER: olegabu
+// GITHUB_SSH_CREDENTIALS_ID: github
+
+// FABRIC_VERSION: 1.4
+
+
+
+
+def evaluateNextSnapshotGitTag(repoTitle) {
+    echo "Evaluate next snapshot name for ${repoTitle}"
+    def lastSnapshot = sh(returnStdout: true, script: "git branch -r --list 'origin/snapshot-*' --sort=-committerdate | head -1").trim()
+    echo "Current latest snapshot: ${lastSnapshot}"
+    def (branchPrefix, version, fabricVersion) = lastSnapshot.tokenize("-")
+    def (majorVer, minorVer) = version.tokenize(".")
+    int minorVersion = (minorVer as int)
+    def newGitTag = "${branchPrefix}-${majorVer}.${minorVersion + 1}-${fabricVersion}"
+
+    newTag = newGitTag.split("/")[1]
+    echo "New Tag for ${repoTitle}: ${newTag}"
+    newTag
+}
 
 node {
 
-    stage('fabric-starter-rest') {
-        stage('Checkout') {
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+    stage('Fabric-Starter-Rest') {
+        def newFabricStarterRestTag
+        stage('Checkout Fabric-starter-rest') {
+            checkout([$class                           : 'GitSCM', branches: [[name: '*/master']],
                       doGenerateSubmoduleConfigurations: false, submoduleCfg: [],
-                      extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'rest']],
-                      userRemoteConfigs: [[credentialsId: 'test', url: "${REST_REPO}"]]])
+                      extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'fabric-starter-rest']],
+                      userRemoteConfigs                : [[credentialsId: "${GITHUB_SSH_CREDENTIALS_ID}", url: "git@github.com:${GIT_REPO_OWNER}/fabric-starter-rest.git"]]])
         }
-        stage("Build Stable") {
-            echo "${PWD}"
-
-            dir("rest") {
-                sh "ls -l"
-                def lastSnapshot=sh(returnStdout: true, script: "git branch -r --list 'origin/snapshot-*' --sort=-committerdate | head -1").trim()
-                echo lastSnapshot
-                def (branchPrefix, version, fabricVersion) = lastSnapshot.tokenize("-")
-                echo version
-                def (ver0, ver1)=version.tokenize(".")
-                int v1=(ver1 as int)
-                def newBranch="${branchPrefix}-${ver0}.${v1+1}-${fabricVersion}"
-                echo newBranch
-
-                def image
-                docker.withRegistry('https://docker.io', 'dockerhub') {
-                    image = docker.image('vrreality/fabric-starter-rest')
+        stage("Build Stable docker image of fabric-starter-rest") {
+            dir("fabric-starter-rest") {
+                echo "PWD: ${PWD}"
+                newFabricStarterRestTag = evaluateNextSnapshotGitTag('Fabric-starter-rest')
+                docker.withRegistry("${DOCKER_REGISTRY}", "${DOCKER_CREDENTIALS_ID}") {
+                    def image = docker.image('${DOCKER_REPO}/fabric-starter-rest:latest')
                     image.pull()
-                    sh 'docker tag private-registry-1/my-image:tag private-registry-2/my-image:tag'
+                    sh "docker tag ${DOCKER_REPO}/fabric-starter-rest:latest ${DOCKER_REPO}/fabric-starter-rest:${newFabricStarterRestTag}"
+                    image = docker.image("${DOCKER_REPO}/fabric-starter-rest:${newFabricStarterRestTag}")
+                    //image.push()
                 }
-
-
+            }
+        }
+        stage('Snapshot fabric-starter-rest') {
+            dir('fabric-starter-rest') {
+                sshagent(credentials: ["${GITHUB_SSH_CREDENTIALS_ID}"]) {
+                    sh "git checkout -B ${newFabricStarterRestTag}"
+                    sh("git push -u origin ${newFabricStarterRestTag}")
+                }
             }
 
         }
-        stage('fabric-starter') {
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-                      doGenerateSubmoduleConfigurations: false, submoduleCfg: [],
-                      extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'starter']],
-                      userRemoteConfigs: [[credentialsId: 'test', url: 'https://github.com/leonidle/fabric-starter']]])
-        }
 
-        stage('Build Stable') {
+        stage('Fabric-Starter-Rest') {
+            stage('Snapshot fabric-starter') {
+                checkout([$class                           : 'GitSCM', branches: [[name: '*/master']],
+                          doGenerateSubmoduleConfigurations: false, submoduleCfg: [],
+                          extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'fabric-starter'], [$class: 'UserIdentity', name: "${GIT_REPO_OWNER}"]],
+                          userRemoteConfigs                : [[credentialsId: "${GITHUB_SSH_CREDENTIALS_ID}", url: "git@github.com:${GIT_REPO_OWNER}/fabric-starter.git"]]])
 
-            echo "${PWD}"
-            script {
-                def lastSnapshot=sh("git branch -r --list 'origin/snapshot-*' --sort=-committerdate | head -1")
-                echo lastSnapshot
-
+                dir('fabric-starter') {
+                    echo "PWD: ${PWD}"
+                    def newFabricStarterTag = evaluateNextSnapshotGitTag('Fabric-starter')
+                    sshagent(credentials: ["${GITHUB_SSH_CREDENTIALS_ID}"]) {
+                        sh "git checkout -B ${newFabricStarterTag}"
+                        def envFileContent = readFile '.env'
+                        writeFile file: '.env', text: "${envFileContent}\nFABRIC_STARTER_REST_VERSION=${newFabricStarterRestTag}\nFABRIC_VERSION=${FABRIC_VERSION}"
+                        sh "git add .env"
+                        sh "git commit -m \'Snapshot ${newFabricStarterTag}\'"
+                        sh("git push -u origin ${newFabricStarterTag}")
+                    }
+                }
             }
         }
-
     }
-
-
 }
+
+
