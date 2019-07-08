b11272baf00b7ee4148b820824e7b3667b3ce28a
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-07-08 12:50:02 +0300

    increase timeout before installing dns cc

--- container-scripts/install-default-chaincodes.sh
+++ container-scripts/install-default-chaincodes.sh
@@ -3,5 +3,5 @@ source ./container-lib.sh 2>/dev/null # for IDE code completion
 source $(dirname "$0")/container-lib.sh
 
 echo -e "\n\nInstall DNS chaincode\n\n"
-sleep 6
+sleep 10
 installChaincode dns /opt/chaincode/node/dns node 1.0
