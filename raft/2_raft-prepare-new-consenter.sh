62e78e1ab8e113bf162ef3c1cbf02dab27650627
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-07-29 20:53:56 +0300

    start multihost

--- raft/2_raft-prepare-new-consenter.sh
+++ raft/2_raft-prepare-new-consenter.sh
@@ -12,6 +12,7 @@ exampleMsg="ORG=org1 ORDERER_NAME=raft0 $0"
 : ${DOMAIN:=example.com}
 : ${ORDERER_NAME:=raft0}
 : ${DOCKER_COMPOSE_ORDERER_ARGS:=-f docker-compose-orderer.yaml}
+: ${WWW_PORT:=81}
 
 COMPOSE_PROJECT_NAME=${ORDERER_NAME}.${DOMAIN} ORDERER_GENESIS_PROFILE=RaftOrdererGenesis docker-compose ${DOCKER_COMPOSE_ORDERER_ARGS} up -d www.orderer
 
