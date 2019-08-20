b0c9d9e879c735e0b78bdf84e821b9ba4ca4c137
Andrew Pudovikov <pudovikov@me.com>
2019-08-20 17:23:24 +0300

    some small fixes

--- chaincode-install-package.sh
+++ chaincode-install-package.sh
@@ -6,4 +6,6 @@ exampleMsg="$0 reference.cds"
 IFS=
 chaincodeName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
 
+: ${PEER0_PORT:=7051}
+
 ORDERER_DOMAIN=${ORDERER_DOMAIN:-$DOMAIN} ORDERER_NAME=${ORDERER_NAME} runCLI "container-scripts/network/chaincode-install-package.sh $chaincodeName"
