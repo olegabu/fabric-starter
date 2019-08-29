a67f58d4fc0409357e7ea65dedd1be660cd19c10
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-08-29 18:07:30 +0300

    wait after create channel

--- channel-create.sh
+++ channel-create.sh
@@ -7,3 +7,5 @@ IFS=
 channelName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
 
 ORDERER_DOMAIN=${ORDERER_DOMAIN:-$DOMAIN} ORDERER_NAME=${ORDERER_NAME} runCLI "container-scripts/network/channel-create.sh $channelName"
+
+sleep 1
