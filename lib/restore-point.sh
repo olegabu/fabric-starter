fa24e1fb88d651d5b4621e8601f9884abd8e0b82
Maxim Fedin <m-fedin@mail.ru>
2019-09-02 19:15:17 +0300

    changes to raft-start and peer-start scripts.

--- lib/restore-point.sh
+++ lib/restore-point.sh
@@ -2,6 +2,9 @@
 cd ..
 source lib.sh
 
+USER_ID=`id -u`
+USER_GRP=`id -g`
+
 usageMsg="$0 backupLabel"
 exampleMsg="$0 1"
 
@@ -13,14 +16,21 @@ backupDir="$PWD/backup/$backupLabel"
 printInColor "1;32" "Using backup from $backupDir"
 
 docker rm -f tempCopy 2>/dev/null
-docker run --name tempCopy --detach -v /var/lib/docker:/docker -v ${backupDir}:/backup olegabu/fabric-tools-extended bash -c "tail -f /var/log/dpkg.log"
-
+docker run --name tempCopy --detach \
+-v /var/lib/docker:/docker \
+-v data:/opt/data \
+-v crypto-config:/opt/crypto-config \
+-v ${backupDir}:/backup \
+olegabu/fabric-tools-extended bash -c "tail -f /var/log/dpkg.log"
 
 printInColor "1;32" "\nRestoring files"
 docker exec -t tempCopy bash -c "rm -rf /docker/volumes/*"
 docker exec -t tempCopy bash -c "cp -r -a -f /backup/volumes /docker"
 docker exec -t tempCopy bash -c "ls /docker/volumes"
+docker exec -t tempCopy bash -c "chown -v ${USER_ID} -r /opt/crypto-config"
+docker exec -t tempCopy bash -c "chown -v ${USER_ID} -r /opt/data"
+
 docker rm -f tempCopy 1>/dev/null
 
-sudo cp -r -a -f ${backupDir}/crypto-config/ ./
-sudo cp -r -a -f ${backupDir}/data/ ./
+cp -r -a ${backupDir}/crypto-config/ ./
+cp -r -a ${backupDir}/data/ ./
