0ecdbdd910c48c820a3f3e1e83b4739a9281d5f3
Maxim Fedin <m-fedin@mail.ru>
2019-09-02 19:15:17 +0300

    changes to raft-start and peer-start scripts.

--- lib/backup-point.sh
+++ lib/backup-point.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 cd ..
 source lib.sh
+
+USER_ID=`id -u`
+USER_GRP=`id -g`
+
 usageMsg="$0 backupLabel"
 exampleMsg="$0 1"
 
@@ -14,9 +18,14 @@ echo; printInColor "1;32" "Stopping containers"
 docker stop $(docker ps -aq)
 
 echo; printInColor "1;32" "Backuping files"
-docker run --rm -v /var/lib/docker:/docker -v ${backupDir}:/backup olegabu/fabric-tools-extended bash -c "cp -r -a /docker/volumes /backup"
-sudo cp -r -a crypto-config ${backupDir}
-sudo cp -r -a data ${backupDir}
+
+docker run --rm \
+-v /var/lib/docker:/docker \
+-v ${backupDir}:/backup \
+-v data:/opt/data \
+-v crypto-config:/opt/crypto-config \
+olegabu/fabric-tools-extended bash \
+-c "cp -r -a /docker/volumes /backup && chown ${USER_ID}:${USER_ID} -R /opt/crypto-config && chown ${USER_ID}:${USER_ID} -R /opt/data && rm -rf opt/data/ldap/*/certs/* && cp -r -a /opt/data /backup && cp -r -a /opt/crypto-config /backup "
 
 echo; printInColor "1;32" "Starting containers up"
 docker start $(docker ps -aq)
