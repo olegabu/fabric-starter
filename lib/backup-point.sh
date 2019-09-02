7fee82f9c5acad9d344a00754e84ae5f488f84ad
Maxim Fedin <m-fedin@mail.ru>
2019-09-02 12:39:49 +0300

    backup support LDAP backup.

--- lib/backup-point.sh
+++ lib/backup-point.sh
@@ -5,27 +5,18 @@ usageMsg="$0 backupLabel"
 exampleMsg="$0 1"
 
 backupLabel=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
-
-
-
 backupDir="$PWD/backup/$backupLabel"
 
 sudo rm -r -f "$backupDir"
 mkdir -p "$backupDir"
 
 echo; printInColor "1;32" "Stopping containers"
-
 docker stop $(docker ps -aq)
 
 echo; printInColor "1;32" "Backuping files"
 docker run --rm -v /var/lib/docker:/docker -v ${backupDir}:/backup olegabu/fabric-tools-extended bash -c "cp -r -a /docker/volumes /backup"
 sudo cp -r -a crypto-config ${backupDir}
+sudo cp -r -a data ${backupDir}
 
 echo; printInColor "1;32" "Starting containers up"
 docker start $(docker ps -aq)
-
-
-
-
-
-
--- lib/restore-point.sh
+++ lib/restore-point.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-source ../lib.sh
+cd ..
+source lib.sh
 
 usageMsg="$0 backupLabel"
 exampleMsg="$0 1"
 
 backupLabel=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
-cd ..
 
 backupDir="$PWD/backup/$backupLabel"
 [ ! -d "$backupDir" ] && printRedYellow "Directory $backupDir doesn't exist" && exit 1
 
-
 printInColor "1;32" "Using backup from $backupDir"
 
-printInColor "1;32" "\nStopping containers"
-
-docker stop $(docker ps -aq)
 docker rm -f tempCopy 2>/dev/null
 docker run --name tempCopy --detach -v /var/lib/docker:/docker -v ${backupDir}:/backup olegabu/fabric-tools-extended bash -c "tail -f /var/log/dpkg.log"
 
@@ -27,11 +23,4 @@ docker exec -t tempCopy bash -c "ls /docker/volumes"
 docker rm -f tempCopy 1>/dev/null
 
 sudo cp -r -a -f ${backupDir}/crypto-config/ ./
-
-printInColor "1;32" "\nStarting containers"
-docker start $(docker ps -aq)
-
-
-
-
-
+sudo cp -r -a -f ${backupDir}/data/ ./
