7e59996c7b9f542e4d52a20f67900329ef561f71
Maxim Fedin <m-fedin@mail.ru>
2019-10-18 18:28:59 +0300

    ignore added; remove unnecessary script.

--- prepareTokenTransferChaincode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf webapp/*
-
-cp fabric-token-transfer-webapp/index.html webapp
-cp -r fabric-token-transfer-webapp/favicon.ico webapp/
-cp -r fabric-token-transfer-webapp/scripts  webapp/
-cp -r fabric-token-transfer-webapp/font-awesome/ webapp/font-awesome/
-cp -r fabric-token-transfer-webapp/src/locales webapp/src
