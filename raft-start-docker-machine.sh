1a049274995f4065e26049a34e862e22bcb92fde
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-10-29 22:11:27 +0300

    dns middleware from rest server

---
+++ raft-start-docker-machine.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+source lib.sh
+
+
+#domain1=${2:-example.com}
+#org2=${3:-org2}
+#domain2=${4:-example.com}
+
+
+: ${RAFT0_PORT:=7050}
+: ${RAFT1_PORT:=7150}
+: ${RAFT2_PORT:=7250}
+: ${RAFT_NODES_COUNT:=3}
+
+export RAFT0_PORT RAFT1_PORT RAFT2_PORT RAFT_NODES_COUNT
+
+
+first_org=${1:-org1}
+shift
+
+orgs=$@
+
+machineName=$first_org
+
+BOOTSTRAP_IP=$(getMachineIp ${machineName})
+export BOOTSTRAP_IP
+
+
+connectMachine ${machineName}
+
+./clean.sh
+
+export ${DOCKER_COMPOSE_ORDERER_ARGS:="-f docker-compose-orderer.yaml -f docker-compose-orderer-domain.yaml -f docker-compose-orderer-ports.yaml"}
+printYellow "1_raft-start-3-nodes: Starting 3 raft nodes on Org1:"
+ORDERER_DOMAIN=${first_org}.${DOMAIN} raft/1_raft-start-3-nodes.sh
+sleep 20
+exit
+
+
+for org in ${orgs}
+do
+
+    connectMachine ${org}
+
+done
+
+if [ "$domain1" == "$domain2" ]; then
+    printYellow "Delete WWW container to allow new concenter from same domain start flowlessly"
+    docker rm -f www.${domain1}
+fi
+
+echo -e "\n################# orderer (RAFT0) orderer node for Org2\n"
+
+printYellow "2_raft-prepare-new-consenter.sh: Prepare ORG 2 raft0:"
+DOMAIN=${domain2} ORDERER_NAME=${ORG2_RAFT_NAME_1} raft/2_raft-prepare-new-consenter.sh
+sleep 5
+
+printYellow "3_raft-add-consenter: Add new consenter info to config:"
+DOMAIN=${domain1} raft/3_2_raft-add-consenter.sh ${ORG2_RAFT_NAME_1} ${domain2} ${RAFT0_PORT}
+sleep 5
+
+printYellow "4_raft-start-consenter.sh: Start Org2-raft0, wait for join:"
+# skip restarting as in local deployment it's already started and successfully joined at prepare step
+#ORG=${org2} DOMAIN=${domain2} ORDERER_NAME=${ORG2_RAFT_NAME_1} raft/4_raft-start-consenter.sh www.${domain1}
+echo "Waitng ${ORG2_RAFT_NAME_1}.${domain2}"
+
+sleep 20
+
+printYellow "5_raft-update-endpoints: Include endpoints ${ORG2_RAFT_NAME_1}.${domain2} to the system-channel"
+DOMAIN=${domain1} raft/5_raft-update-endpoints.sh ${ORG2_RAFT_NAME_1} ${domain2} ${RAFT0_PORT}
+sleep 5
+
+echo -e "\n################# RAFT1 orderer node for Org2\n"
+
+if [ "$domain1" == "$domain2" ]; then
+    printYellow "Delete WWW container to allow new consenter from same domain start flowlessly"
+    docker rm -f www.${domain1}
+fi
+printYellow "6. Prepare ORG 2 raft4:"
+DOMAIN=${domain2} ORDERER_NAME=${ORG2_RAFT_NAME_2} ORDERER_GENERAL_LISTENPORT=${RAFT1_PORT} raft/2_raft-prepare-new-consenter.sh
+sleep 5
+
+printYellow "7. raft-add-consenter.sh: to add ${ORG2_RAFT_NAME_2}.${domain2} to the consenters list"
+DOMAIN=${domain1} raft/3_2_raft-add-consenter.sh ${ORG2_RAFT_NAME_2} ${domain2} ${RAFT1_PORT}
+sleep 5
+
+printYellow "8 _raft-start-consenter.sh: Start ${ORG2_RAFT_NAME_2}, wait for join:"
+# skip restarting as in local deployment it's already started and successfully joined at prepare step
+#DOMAIN=${domain2} ORDERER_NAME=${ORG2_RAFT_NAME_2} ORDERER_GENERAL_LISTENPORT=${RAFT1_PORT} raft/4_raft-start-consenter.sh www.${domain1}
+
+echo "Waitng ${ORG2_RAFT_NAME_2}.${domain2}"
+sleep 20
+
+printYellow "4_raft-update-endpoints: Include endpoints raft4.${domain2} to the system-channel"
+DOMAIN=${domain1} raft/5_raft-update-endpoints.sh ${ORG2_RAFT_NAME_2} ${domain2} ${RAFT1_PORT}
+
+
--- raft/1_raft-start-3-nodes.sh
+++ raft/1_raft-start-3-nodes.sh
@@ -7,22 +7,22 @@ source ../lib.sh 2>/dev/null # for IDE code completion
 usageMsg="ORG=org ORDERER_NAME_PREFIX=<orderer name prefix> $0 <new-consenter-name> <new-consenter-org> <new-consenter-domain> <new-consenter-port>"
 exampleMsg="ORG=org1 ORDERER_NAME_PREFIX=raft0 $0"
 
-: ${DOMAIN:=example.com}
+: ${ORDERER_DOMAIN:-${DOMAIN:=example.com}}
 : ${ORDERER_NAME:=orderer}
 : ${ORDERER_NAME_2:=raft1}
 : ${ORDERER_NAME_3:=raft2}
 : ${RAFT_NODES_COUNT:=3}
-: ${DOCKER_COMPOSE_ORDERER_ARGS:="-f docker-compose-orderer.yaml"}
+: ${DOCKER_COMPOSE_ORDERER_ARGS:="-f docker-compose-orderer.yaml -f docker-compose-orderer-domain.yaml"}
 
-export DOMAIN WWW_PORT RAFT_NODES_COUNT
+export DOMAIN ORDERER_DOMAIN WWW_PORT RAFT_NODES_COUNT
 
 echo "Start first Raft node 0 (orderer.$DOMAIN)"
 ORDERER_GENESIS_PROFILE=Raft3OrdererGenesis COMPOSE_PROJECT_NAME=${ORDERER_NAME}.${DOMAIN} ORDERER_GENERAL_LISTENPORT=${RAFT0_PORT:-7050} ORDERER_NAME=${ORDERER_NAME} docker-compose ${DOCKER_COMPOSE_ORDERER_ARGS} up -d orderer cli.orderer www.orderer
 sleep 5
 echo "Start first Raft node 1"
-COMPOSE_PROJECT_NAME=${ORDERER_NAME_2}.${DOMAIN} ORDERER_GENERAL_LISTENPORT=${RAFT1_PORT:-7050} ORDERER_NAME=${ORDERER_NAME_2} docker-compose ${DOCKER_COMPOSE_ORDERER_ARGS} up -d orderer
+COMPOSE_PROJECT_NAME=${ORDERER_NAME_2}.${DOMAIN} ORDERER_GENERAL_LISTENPORT=${RAFT1_PORT:-7150} ORDERER_NAME=${ORDERER_NAME_2} docker-compose ${DOCKER_COMPOSE_ORDERER_ARGS} up -d orderer
 sleep 5
 echo "Start first Raft node 2"
-COMPOSE_PROJECT_NAME=${ORDERER_NAME_3}.${DOMAIN} ORDERER_GENERAL_LISTENPORT=${RAFT2_PORT:-7050} ORDERER_NAME=${ORDERER_NAME_3} docker-compose ${DOCKER_COMPOSE_ORDERER_ARGS} up -d orderer
+COMPOSE_PROJECT_NAME=${ORDERER_NAME_3}.${DOMAIN} ORDERER_GENERAL_LISTENPORT=${RAFT2_PORT:-7250} ORDERER_NAME=${ORDERER_NAME_3} docker-compose ${DOCKER_COMPOSE_ORDERER_ARGS} up -d orderer
 sleep 5
 
