cf0b02dac79255b385dd5800d1c77a93edb6c029
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-10-29 21:20:27 +0300

    dns middleware from rest server

--- middleware/dns.js
+++ middleware/dns.js
@@ -3,38 +3,53 @@
 // verify ip was added to hosts file and the name is now resolvable
 // docker exec api.org1.example.com wget peer0.org2.example.com
 
-module.exports = async app => {
+module.exports = async (app, _fabricStarterClient, eventBus) => {
     const fs = require('fs');
     const _ = require('lodash');
     const logger = require('log4js').getLogger('dns');
+    const jwt = require('jsonwebtoken');
+
     const FabricStarterClient = require('../fabric-starter-client');
-    const restSocketServer = require('../rest-socket-server');
     const fabricStarterClient = new FabricStarterClient();
+    const util = require('../util');
+    const cfg = require('../config.js');
 
     const NODE_HOSTS_FILE = '/etc/hosts';
     const ORDERER_HOSTS_FILE = '/etc/hosts_orderer';
 
     const channel = process.env.DNS_CHANNEL || 'common';
     const chaincodeName = process.env.DNS_CHAINCODE || 'dns';
-    const username = process.env.DNS_USERNAME || 'dns';
-    const password = process.env.DNS_PASSWORD || 'pass';
-    const skip = !process.env.MULTIHOST;
+    const username = process.env.DNS_USERNAME || 'serviceUser';
+    const password = process.env.DNS_PASSWORD || 'servicePass';
+    // const skip = !process.env.MULTIHOST;
     const period = process.env.DNS_PERIOD || 60000;
     const orgDomain = `${process.env.ORG}.${process.env.DOMAIN}`;
     const myIp = process.env.MY_IP;
     const ordererDomain = process.env.ORDERER_DOMAIN || process.env.DOMAIN;
-    const queryTarget = process.env.DNS_QUERY_TARGET || `peer0.${orgDomain}:7051`;
+    const queryTarget = process.env.DNS_QUERY_TARGET || `peer0.${orgDomain}:${process.env.PEER0_PORT || 7051}`;
 
-    let dnsUserLoggedin = false;
+
     let blockListenerStarted = false;
-
-    // app.get('/block-logger', (req, res) => {
-    //   res.status(200).send(`Welcome to block-logger. Channels: ${channels}`);
-    // });
 
     logger.info('started');
 
-    await processEvent();
+    let taskSettings = {};
+    let orgs = {};
+
+    app.get("/settings/tasks", (req, res) => {
+        res.json(taskSettings);
+    });
+
+    app.post("/settings/tasks", (req, res) => {
+        logger.info('Saving tasks');
+
+        taskSettings = req.body;
+        // eventBus.emit('orgs-configuration-changed', orgs);
+        // eventBus.emit('osn-configuration-changed');
+        // const dnsResponses = await fabricStarterClient.query(channel, chaincodeName, 'get', '["dns"]', {targets: queryTarget});
+        res.status(200).send("");
+    });
+
 
     setInterval(async () => {
         logger.info(`periodically query every ${period} msec for dns entries and update ${NODE_HOSTS_FILE}`);
@@ -45,68 +60,17 @@ module.exports = async app => {
         }
     }, period);
 
-
-    async function login() {
-        await fabricStarterClient.init();
-        await fabricStarterClient.loginOrRegister(username, password);
-
-        logger.info(`logged in as ${username}`);
-    }
-
-    async function startBlockListener() {
-        logger.info(`logged in as ${username}`);
-
-        logger.info(`Trying to start DNS block listener on channel ${channel}`);
-        await fabricStarterClient.registerBlockEvent(channel, async block => {
-            logger.debug(`block ${block.number} on ${block.channel_id}`);
-
-            await processEvent();
-        }, e => {
-            throw new Error(e);
-        });
-    }
-
-    function existsAndIsFile(file) {
-        try {
-            fs.accessSync(file, fs.constants.W_OK);
-            return fs.statSync(file).isFile()
-        } catch (e) {
-            logger.debug(`Cannot open file ${file}`, e);
-        }
-    }
-
-    function writeFile(file, hostsFileContent) {
-        if (existsAndIsFile(file)) {
-            try {
-                fs.writeFileSync(file, hostsFileContent);
-                logger.info(`written: ${file}\n`, hostsFileContent);
-            } catch (err) {
-                logger.error(`cannot writeFile ${file}`, err);
-            }
-        } else {
-            logger.debug(`Skipping ${file}`);
-        }
-    }
+    await processEvent();
 
     async function processEvent() {
-        if (skip) {
-            logger.info('skipping event');
-            return;
-        }
-
-        if (!dnsUserLoggedin) {
-            try {
-                await login();
-                dnsUserLoggedin = true;
-            } catch (e) {
-                logger.warn("Can't start DNS block listener", e);
-                return;
-            }
-        }
-
+        // if (skip) {
+        //     logger.info('Skipping dns track as not MULTIHOST');
+        //     return;
+        // }
 
         if (!blockListenerStarted) {
             try {
+                await login();
                 await startBlockListener();
                 blockListenerStarted = true;
             } catch (e) {
@@ -115,63 +79,66 @@ module.exports = async app => {
             }
         }
 
-
-        const queryResponses = await fabricStarterClient.query(channel, chaincodeName, 'get', '["dns"]', {targets: queryTarget});
-        logger.debug('queryResponses', queryResponses);
-
-        const ret = queryResponses;//[0];
-
-        if (ret) {
-            let list;
-
-            try {
-                list = JSON.parse(ret);
-            } catch (e) {
-                logger.warn("Unparseable dns-records: \n", ret);
-            }
-
-            if (!list && !list.length) {
-                return logger.debug('no dns records found on chain');
-            }
-
-            // list = filterOutOrgByPattern(list, `.${orgDomain}`);
-            list = filterOutByIp(list, myIp);
-            logger.debug("DNS after org filtering", list);
-
-            // if (existsAndIsFile(ORDERER_HOSTS_FILE)) {
-            //     list = filterOutOrgByPattern(list, `orderer.${ordererDomain}`);
-            // }
-
-            // logger.debug("DNS after org and orderer filtering", list);
-
-            let hostsFileContent = generateHostsRecords(list);
-
-            writeFile(NODE_HOSTS_FILE, hostsFileContent);
-            writeFile(ORDERER_HOSTS_FILE, hostsFileContent);
+        let dnsRecords = await getChaincodeData("dns");
+        if (dnsRecords) {
+            dnsRecords = filterOutByIp(dnsRecords, myIp);
+            util.writeFile(NODE_HOSTS_FILE, dnsRecords);
+            util.writeFile(ORDERER_HOSTS_FILE, dnsRecords);
         }
+
+        const osns = await getChaincodeData("osn");
+        if (osns) {
+            eventBus.emit('osn-configuration-changed', osns);
+        }
+
+        const chainOrgs = await getChaincodeData("orgs");
+        if (chainOrgs) {
+            orgs = _.merge(orgs, chainOrgs);
+            // eventBus.emit('orgs-configuration-changed', orgs);
+            // eventBus.emit('osn-configuration-changed', osns);
+        }
+
+
+        // taskSettigns=await getChaincodeData("tasksSettings") ||{};
+
+    }
+
+    async function login() {
+        await fabricStarterClient.init();
+        await fabricStarterClient.loginOrRegister(username, password);
+
+        logger.info(`logged in as ${username}`);
+    }
+
+
+    async function startBlockListener() {
+        logger.info(`logged in as ${username}`);
+
+        logger.info(`Trying to start DNS block listener on channel ${channel}`);
+        await fabricStarterClient.registerBlockEvent(channel, async block => {
+            logger.debug(`block ${block.number} on ${block.channel_id}`);
+            await processEvent();
+        }, e => {
+            throw new Error(e);
+        });
+    }
+
+    async function getChaincodeData(dataKey) {
+        let result = null;
+        const dataResponses = await fabricStarterClient.query(channel, chaincodeName, 'get', `["${dataKey}"]`, {targets: queryTarget});
+        logger.debug(`dataResponses for ${dataKey}`, dataResponses);
+        try {
+            if (dataResponses && dataResponses[0] !== '') {
+                result = JSON.parse(dataResponses);
+            }
+        } catch (e) {
+            logger.warn(`Can't parse chaincode data record: ${dataResponses}`, e);
+        }
+        return result;
     }
 
     function filterOutByIp(list, ip) {
-        return list.filter(o => _.get(o, "key") !== ip);
-    }
-
-    function generateHostsRecords(list) {
-        let h = `# replaced by dns listener on ${channel}\n`;
-        list.forEach(o => {
-            h = h.concat(o.key, ' ', o.value, '\n');
-        });
-        return h;
-    }
-
-
-    function filterOutOrgByPattern(list, pattern) {
-        list.forEach(o => {
-            logger.debug(`Filtering ${_.get(o, "value")} with ${pattern}`);
-        });
-
-        list = list.filter(o => !_.includes(_.get(o, "value"), `${pattern}`));
+        delete list[ip];
         return list;
     }
-
-
 };
