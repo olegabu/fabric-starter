a53c494999ff8b1e23052883c4b52c1e30a74f90
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-12-03 12:00:15 +0300

    main.sh for baas start

--- main.sh
+++ main.sh
@@ -4,17 +4,13 @@ function info() {
     echo -e "************************************************************\n\033[1;33m${1}\033[m\n************************************************************"
 }
 
-export DOMAIN=${DOMAIN:-example.com}
-
-orgs=${@:-org1}
+orgs=$@
 first_org=${1:-org1}
 
-channel=${CHANNEL:-common}
-chaincode_install_args=${CHAINCODE_INSTALL_ARGS-reference}
-chaincode_instantiate_args=${CHAINCODE_INSTANTIATE_ARGS:-common reference}
-docker_compose_args=${DOCKER_COMPOSE_ARGS:- -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-dev.yaml}
+export DOMAIN=${DOMAIN:-example.com}
+export SERVICE_CHANNEL=${SERVICE_CHANNEL:-common}
 
-# Clean up. Remove all containers, delete local crypto material
+docker_compose_args=${DOCKER_COMPOSE_ARGS:- -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-api-port.yaml -f environments/dev/docker-compose-debug.yaml}
 
 info "Cleaning up"
 ./clean.sh
@@ -23,98 +19,32 @@ unset ORG COMPOSE_PROJECT_NAME
 # Create orderer organization
 
 info "Creating orderer organization for $DOMAIN"
-export ORDERER_WWW_PORT=79
+ORDERER_WWW_PORT=79
 source ${first_org}_env
 
-docker pull ${DOCKER_REGISTRY:-docker.io}/olegabu/fabric-tools-extended:${FABRIC_STARTER_VERSION:-latest}; \
-docker pull ${DOCKER_REGISTRY:-docker.io}/olegabu/fabric-starter-rest:${FABRIC_STARTER_VERSION:-latest}; \
+#docker pull ${DOCKER_REGISTRY:-docker.io}/olegabu/fabric-tools-extended:${FABRIC_STARTER_VERSION:-latest}
+#docker pull ${DOCKER_REGISTRY:-docker.io}/olegabu/fabric-starter-rest:${FABRIC_STARTER_VERSION:-latest}
 
-WWW_PORT=${ORDERER_WWW_PORT} docker-compose -f docker-compose-orderer.yaml -f docker-compose-orderer-ports.yaml up -d
+WWW_PORT=${ORDERER_WWW_PORT} docker-compose -f docker-compose-orderer.yaml -f docker-compose-orderer-ports.yaml -f environments/dev/docker-compose-orderer-debug.yaml up -d
 
-# Create member organizations
-
-api_port=${API_PORT:-4000}
-#dev:
-www_port=${WWW_PORT:-81}
-ca_port=${CA_PORT:-7054}
-peer0_port=${PEER0_PORT:-7051}
-#
 
 ldap_http=${LDAP_PORT_HTTP:-6080}
 ldap_https=${LDAP_PORT_HTTPS:-6443}
 
-custom_port=${CUSTOM_PORT}
 
-
-for org in ${orgs}
-do
-    export ORG=${org} API_PORT=${api_port} WWW_PORT=${www_port} PEER0_PORT=${peer0_port} CA_PORT=${ca_port} LDAP_PORT_HTTP=${ldap_http} LDAP_PORT_HTTPS=${ldap_https} CUSTOM_PORT=${custom_port}
-    export COMPOSE_PROJECT_NAME=${ORG}
-    info "Creating member organization $ORG with api $API_PORT"
-    echo "docker-compose ${docker_compose_args} up -d"
+for org in ${orgs}; do
 
     source ${org}_env
-
-    docker-compose ${docker_compose_args} up -d
-    api_port=$((api_port + 1))
-    www_port=$((www_port + 1))
-    ca_port=$((ca_port + 1))
-    peer0_port=$((peer0_port + 1000))
-    ldap_http=$((ldap_http + 100))
-    ldap_https=$((ldap_https + 100))
-    custom_port=$((custom_port + 1))
-    unset ORG COMPOSE_PROJECT_NAME API_PORT WWW_PORT PEER0_PORT CA_PORT LDAP_PORT_HTTP LDAP_PORT_HTTPS CUSTOM_PORT
+    info "Creating member organization $ORG with api $API_PORT"
+    echo "docker-compose ${docker_compose_args} up -d"
+    COMPOSE_PROJECT_NAME=${org} docker-compose ${docker_compose_args} up -d
 done
 
-# Add member organizations to the consortium
+docker wait post-install.${first_org}.${DOMAIN}
 
-for org in "${@:2}"
-do
-    info "Adding $org to the consortium"
-    ./consortium-add-org.sh ${org}
+for org in "${@:2}"; do
+    source ${org}_env
+    info "Adding $org to channel ${SERVICE_CHANNEL}"
+    COMPOSE_PROJECT_NAME=${org} ORG=$first_org ./channel-add-org.sh ${SERVICE_CHANNEL} ${org} ${PEER0_PORT}
 done
 
-# First organization creates the channel
-
-export ORG=${first_org}
-export COMPOSE_PROJECT_NAME=${ORG}
-
-# Wait for container scripts completed
-info "Wait for post-install.${ORG}.${DOMAIN} completed"
-#docker wait post-install.${ORG}.${DOMAIN}
-sleep 20
-
-# First organization adds other organizations to the channel
-
-peer0_port=$((${PEER0_PORT:-7051}+1000))
-
-for org in "${@:2}"
-do
-    info "Adding $org to channel ${channel}"
-    ./channel-add-org.sh ${channel} ${org} ${peer0_port}
-    peer0_port=$((peer0_port + 1000))
-done
-
-# First organization creates the chaincode
-if [ -n "${chaincode_install_args}" ]; then
-   info "Creating chaincode by $ORG: ${chaincode_install_args} ${chaincode_instantiate_args}"
-   ./chaincode-install.sh ${chaincode_install_args}
-   ./chaincode-instantiate.sh ${chaincode_instantiate_args}
-fi
-
-# Other organizations join the channel
-
-peer0_port=${PEER0_PORT:-7051}
-
-for org in "${@:2}"
-do
-    export ORG=${org}
-    export COMPOSE_PROJECT_NAME=${ORG}
-    peer0_port=$((peer0_port + 1000))
-    info "Joining $org to channel ${channel}"
-    PEER0_PORT=$peer0_port ./channel-join.sh ${channel}
-    if [ -n "${chaincode_install_args}" ]; then
-        PEER0_PORT=$peer0_port ./chaincode-install.sh ${chaincode_install_args}
-    fi
-    unset ORG COMPOSE_PROJECT_NAME
-done
