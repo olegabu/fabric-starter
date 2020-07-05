a4e22185e4386c7be8a5c67f404fff41c4b6a40b
leonidle <1@24443.com>
2020-07-05 19:36:11 +0300

    bootstrap deployment

--- main.sh
+++ main.sh
@@ -7,8 +7,8 @@ function info() {
 orgs=$@
 first_org=${1:-org1}
 
-#export BOOTSTRAP_IP=${BOOTSTRAP_IP:-37.18.119.176}
-export BOOTSTRAP_IP=${BOOTSTRAP_IP:-37.18.72.69}
+export BOOTSTRAP_IP=${BOOTSTRAP_IP:-37.18.119.176}
+#export BOOTSTRAP_IP=${BOOTSTRAP_IP:-37.18.72.69}
 export DOMAIN=${DOMAIN:-example.com}
 export SERVICE_CHANNEL=${SERVICE_CHANNEL:-common}
 
