6892f1076de543f492a46a46f71230c5dff8b8d5
leonidle <1@24443.com>
2020-07-05 15:32:22 +0300

    bootstrap deployment

--- main.sh
+++ main.sh
@@ -72,28 +72,28 @@ shopt -u nocasematch
 sleep 3
 
 
-#info "Create first organization ${first_org}"
-#echo "docker-compose ${docker_compose_args} up -d"
-#source ${first_org}_env;
-#COMPOSE_PROJECT_NAME=${first_org} docker-compose ${docker_compose_args} up -d
-#
-#echo -e "\nWait post-install.${first_org}.${DOMAIN} to complete"
-#docker wait post-install.${first_org}.${DOMAIN} > /dev/null
-#
-#for org in ${@:2}; do
-#    source ${org}_env
-#    info "      Creating member organization $ORG with api $API_PORT"
-#    echo "docker-compose ${docker_compose_args} up -d"
-#    COMPOSE_PROJECT_NAME=${org} docker-compose ${docker_compose_args} up -d
-#done
-#
-#sleep 4
-#for org in "${@:2}"; do
-#    source ${org}_env
-#    orgPeer0Port=${PEER0_PORT}
-#
-#    info "Adding $org to channel ${SERVICE_CHANNEL}"
-#    source ${first_org}_env;
-#    COMPOSE_PROJECT_NAME=$first_org ORG=$first_org ./channel-add-org.sh ${SERVICE_CHANNEL} ${org} ${orgPeer0Port}
-#done
+info "Create first organization ${first_org}"
+echo "docker-compose ${docker_compose_args} up -d"
+source ${first_org}_env;
+COMPOSE_PROJECT_NAME=${first_org} docker-compose ${docker_compose_args} up -d
+
+echo -e "\nWait post-install.${first_org}.${DOMAIN} to complete"
+docker wait post-install.${first_org}.${DOMAIN} > /dev/null
+
+for org in ${@:2}; do
+    source ${org}_env
+    info "      Creating member organization $ORG with api $API_PORT"
+    echo "docker-compose ${docker_compose_args} up -d"
+    COMPOSE_PROJECT_NAME=${org} docker-compose ${docker_compose_args} up -d
+done
+
+sleep 4
+for org in "${@:2}"; do
+    source ${org}_env
+    orgPeer0Port=${PEER0_PORT}
+
+    info "Adding $org to channel ${SERVICE_CHANNEL}"
+    source ${first_org}_env;
+    COMPOSE_PROJECT_NAME=$first_org ORG=$first_org ./channel-add-org.sh ${SERVICE_CHANNEL} ${org} ${orgPeer0Port}
+done
 
