b204c211b4740e9d538b2df55e3e4a7858faf4e0
leonidle <>
2020-09-26 23:38:35 +0300

    workaround for disk resize until fixed

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # workaround for disk resize
+sleep 10
 sudo lvextend /dev/rootvg/root /dev/vda3 #extend virtual group
 sleep 5
 sudo resize2fs /dev/rootvg/root #resize file system
