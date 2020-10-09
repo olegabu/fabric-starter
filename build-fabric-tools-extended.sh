4b6da7cb1ea41a1e641336bf825b4560001921c1
alkilpio <59166264+alkilpio@users.noreply.github.com>
2020-10-09 12:45:55 +0300

    repository parametrization added (#117)
    * repository parametrization added

--- build-fabric-tools-extended.sh
+++ build-fabric-tools-extended.sh
@@ -1,1 +1,5 @@
-docker build -t olegabu/fabric-tools-extended -f ./fabric-tools-extended/Dockerfile --no-cache --build-arg FABRIC_VERSION=${FABRIC_VERSION:-latest} .
+#!/usr/bin/env bash
+FABRIC_STARTER_REPOSITORY=${FABRIC_STARTER_REPOSITORY:-olegabu}
+FABRIC_VERSION=${FABRIC_VERSION:-latest}
+
+docker build -t ${FABRIC_STARTER_REPOSITORY}/fabric-tools-extended -f ./fabric-tools-extended/Dockerfile --no-cache --build-arg FABRIC_VERSION=${FABRIC_VERSION} --build-arg FABRIC_STARTER_REPOSITORY=${FABRIC_STARTER_REPOSITORY} .
