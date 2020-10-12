e8e382a7e973243524426a81e89347ace2361e7d
leonidle <>
2020-10-12 14:11:57 +0300

    v3 exts added

--- container-scripts/tls-certs/key-gen.sh
+++ container-scripts/tls-certs/key-gen.sh
@@ -7,8 +7,8 @@ function main() {
     generateRootTlsCert 'tls-orderer/tls-root' example.com tlsca.example.com
     generateRootTlsCert 'tls-org1/tls-root' org1.example.com tlsca.org1.example.com
 
-    generateRootTlsCert 'tls-orderer/tls' example.com orderer.example.com
-    generateRootTlsCert 'tls-org1/tls' org1.example.com peer0.org1.example.com
+#    generateRootTlsCert 'tls-orderer/tls' example.com orderer.example.com v3_tls
+#    generateRootTlsCert 'tls-org1/tls' org1.example.com peer0.org1.example.com v3_tls
 
     signCert 'tls-orderer' orderer.example.com
     signCert 'tls-org1' peer0.org1.example.com
@@ -33,10 +33,11 @@ function generateRootTlsCert() {
 
 
     openssl req \
-       -newkey rsa:4096 -nodes -keyout ${path}/server.key \
-       -x509 -days 365 -out ${path}/server.crt -sha256 \
+       -newkey rsa:2048 -nodes -keyout ${path}/server.key \
+       -x509 -days 365 \
        -config ./tlsca-csr.conf \
-       -extensions v3_ca
+       -extensions v3_ca \
+       -out ${path}/server.crt -sha256
 
 }
 
@@ -45,13 +46,20 @@ function signCert() {
     local entityPath=${1:?entityPath required}
     local cnName=${2:?cn required}
 
-    openssl req -nodes -newkey rsa:4096 -keyout ${entityPath}/tls/server.key -out ${entityPath}/server.csr \
+    mkdir -p ${entityPath}/tls
+
+set -x
+    openssl req -nodes -newkey rsa:2048 -keyout ${entityPath}/tls/server.key -out ${entityPath}/server.csr \
         -subj "/C=US/ST=California/L=San Francisco/CN=${cnName}"
 
     openssl x509 -req -in ${entityPath}/server.csr -CA ${entityPath}/tls-root/server.crt \
         -CAkey ${entityPath}/tls-root/server.key \
-        -CAcreateserial -out ${entityPath}/tls/server.crt
+        -CAcreateserial \
+        -extfile ./tlsca-csr.conf  \
+        -extensions v3_tls \
+        -out ${entityPath}/tls/server.crt -sha256
 
+set +x
 }
 
 main
