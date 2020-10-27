b408621e58624f7ed106a9b076a42effd4da487c
leonidle <>
2020-10-27 13:22:18 +0300

    fix params

--- container-scripts/network/channel-add-org.sh
+++ container-scripts/network/channel-add-org.sh
@@ -4,8 +4,8 @@ BASEDIR=$(dirname "$0")
 source container-scripts/lib/container-lib.sh
 source ../lib/container-lib.sh 2>/dev/null # for IDE code completion
 
-channelName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
-newOrg=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
+channelName=${1:?channel name is required}
+newOrg=${2:?new org is required}
 newOrgAnchorPeerPort=${3:-7051}
 newOrgDomain=${4:-$DOMAIN}
 
