470731ff5451b0c61127ded511bbb7a4e6d7c60a
leonidle <>
2021-01-28 17:12:50 +0300

    device names for resize workaround

--- disk_resize.sh
+++ disk_resize.sh
@@ -2,6 +2,6 @@
 
 # workaround for disk resize
 sleep 1
-sudo lvextend /dev/rootvg/root /dev/vda3 #extend virtual group
+sudo lvextend /dev/ubuntu-vg/ubuntu-lv /dev/sda3 #extend virtual group
 sleep 5
-sudo resize2fs /dev/rootvg/root #resize file system
+sudo resize2fs /dev/ubuntu-vg/ubuntu-lv #resize file system
