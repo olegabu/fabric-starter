cd54f63a7732289b41b498f8176360f3b12b7872
kilpio <kilpio@gmail.com>
2021-02-10 12:32:54 +0300

    change FABRIC_VERSION:-latest to 1.4.9

--- build-fabric-tools-extended.sh
+++ build-fabric-tools-extended.sh
@@ -1,1 +1,1 @@
-docker build -t olegabu/fabric-tools-extended:${FABRIC_STARTER_VERSION:-latest} -f ./fabric-tools-extended/Dockerfile --no-cache --build-arg FABRIC_VERSION=${FABRIC_VERSION:-latest} .
+docker build -t olegabu/fabric-tools-extended:${FABRIC_STARTER_VERSION:-latest} -f ./fabric-tools-extended/Dockerfile --no-cache --build-arg FABRIC_VERSION=${FABRIC_VERSION:-1.4.9} .
