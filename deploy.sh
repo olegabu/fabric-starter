e145bbded850ebe8e92d9e30fda37409969a950e
leonidle <>
2021-03-18 01:41:53 +0300

    portainer

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ export FABRIC_STARTER_VERSION=${FABRIC_STARTER_VERSION:-baas-test}
 
 : ${DOCKER_COMPOSE_ORDERER_ARGS:="-f docker-compose-orderer.yaml -f docker-compose-orderer-domain.yaml -f docker-compose-orderer-ports.yaml"}
 
-docker_compose_args=${DOCKER_COMPOSE_ARGS:-"-f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-ldap.yaml -f docker-compose-preload-images.yaml"}
+docker_compose_args=${DOCKER_COMPOSE_ARGS:-"-f docker-compose.yaml -f docker-compose-couchdb.yaml "}
 if [ -n "${HTTPS_MODE}" ]; then # https mode
     export LDAPADMIN_HTTPS=${LDAPADMIN_HTTPS:-true}
     export DOCKER_COMPOSE_EXTRA_ARGS=${DOCKER_COMPOSE_EXTRA_ARGS:-"-f https/docker-compose-https-ports.yaml"}
@@ -64,12 +64,11 @@ if [ -z "${NO_CLEAN}" ]; then
     ./clean.sh all
  fi
 
-set -x
 if [[ -n "${RENEW_IMAGES}" ]]; then
     docker pull ${DOCKER_REGISTRY:-docker.io}/olegabu/fabric-tools-extended:${FABRIC_STARTER_VERSION:-latest}
     docker pull ${DOCKER_REGISTRY:-docker.io}/olegabu/fabric-starter-rest:${FABRIC_STARTER_VERSION:-latest}
 fi
-set +x
+
 
 export COMPOSE_PROJECT_NAME=${ORG:-org1}
 
@@ -81,8 +80,9 @@ fi
 
 info "Create first organization ${ORG}"
 set -x
+docker-compose -f docker-compose-preload-images.yaml up -d
 BOOTSTRAP_IP=${BOOTSTRAP_IP} ENROLL_SECRET="${ENROLL_SECRET:-adminpw}"  docker-compose ${docker_compose_args} ${DOCKER_COMPOSE_EXTRA_ARGS} \
-    up -d ${AGENT_MODE:+ api www.peer}
+    up -d ${AGENT_MODE:+ api www.peer }
 set +x
 
 if [ -z "${AGENT_MODE}" ]; then
