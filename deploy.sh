0a65c70e69df5c249b4009597935905801ae39c5
skilpio <skilpio@gmail.com>
2021-09-06 16:19:43 +0300

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -38,22 +38,6 @@ export FABRIC_STARTER_VERSION=${FABRIC_STARTER_VERSION:-baas-test}
 source ${first_org}_env;
 #export ENROLL_SECRET=`echo ${ENROLL_SECRET/!/\\\\!}`
 
-
-#if [ "$DEPLOY_VERSION" == "Hyperledger Fabric 1.4.4-GOST-34" ]; then
-#    set -x
-#    export DOCKER_REGISTRY=45.12.73.98
-#    export FABRIC_VERSION=gost
-#    export FABRIC_STARTER_VERSION=gost
-#    export AUTH_MODE=ADMIN
-#    export CRYPTO_ALGORITHM=GOST
-#    export SIGNATURE_HASH_FAMILY=SM3
-#
-#    sudo mkdir -p /etc/docker/certs.d/${DOCKER_REGISTRY}
-#    openssl s_client -showcerts -connect ${DOCKER_REGISTRY}:443 </dev/null 2>/dev/null|openssl x509 -outform PEM \
-#        | sudo tee /etc/docker/certs.d/${DOCKER_REGISTRY}/ca.crt
-#    set +x
-#fi
-
 info "Cleaning up"
 ./clean.sh all
 
