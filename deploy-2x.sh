4073061b17345a1441c2e907738247b028bb7563
leonidle <>
2021-09-08 19:11:48 +0300

    support for fabric 2.x

--- deploy-2x.sh
+++ deploy-2x.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export FABRIC_VERSION=${FABRIC_VERSION:-2.3}
+export FABRIC_STARTER_VERSION=${FABRIC_STARTER_VERSION:-2x}
+
 export WGET_CMD="wget -P"
 export BASE64_UNWRAP_CODE="| tr -d '\n'"
 
