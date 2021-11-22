0476055ea84ab8bf3d950ae0c73698b267d54cbc
leonidle <>
2021-11-22 20:59:07 +0300

    custom values set

--- container-scripts/container-orderer.sh
+++ container-scripts/container-orderer.sh
@@ -7,11 +7,15 @@
 : ${CONSENTER_ID:=1}
 : ${ORDERER_PROFILE:=Solo}
 : ${ORDERER_NAME_PREFIX:=raft}
+: ${ORDERER_BATCH_TIMEOUT:=2}
 
-export ORDERER_DOMAIN=${ORDERER_DOMAIN:-$DOMAIN} RAFT_NODES_COUNT RAFT_NODES_NUMBERING_START ORDERER_NAME_PREFIX
+TMP_DIR=${TMP_DIR:-/etc/hyperledger}/crypto-config
+mkdir -p ${TMP_DIR}
+
+export ORDERER_DOMAIN=${ORDERER_DOMAIN:-$DOMAIN} RAFT_NODES_COUNT RAFT_NODES_NUMBERING_START ORDERER_NAME_PREFIX ORDERER_BATCH_TIMEOUT
 
 
-touch crypto-config/hosts
+touch ${TMP_DIR}/hosts
 
 
 function main() {
@@ -25,7 +29,7 @@ function main() {
     copyWellKnownTLSCerts
     copyClientTLSCertForServingByWWW
 
-    tree /etc/hyperledger/crypto-config/ordererOrganizations/
+    tree ${TMP_DIR}/
 }
 
 function constructConfigTxAndCryptogenConfigs() {
@@ -59,25 +63,25 @@ function constructConfigTxAndCryptogenConfigs() {
         done
     fi
 
-    PATTERN=%ORDERER_ADDRESSES% templates/templater.awk crypto-config/configtx_addresses_list.part templates/OrdererProfile_${ORDERER_PROFILE}.yaml > crypto-config/OrdererProfile_1.yaml
-    PATTERN=%ORDERER_ORGS% templates/templater.awk crypto-config/configtx_orgs_list.part crypto-config/OrdererProfile_1.yaml > crypto-config/OrdererProfile_2.yaml
-    PATTERN=%RAFT_CONSENTERS% templates/templater.awk crypto-config/configtx_consenters_list.part crypto-config/OrdererProfile_2.yaml > crypto-config/OrdererProfile.yaml
+    PATTERN=%ORDERER_ADDRESSES% templates/templater.awk ${TMP_DIR}/configtx_addresses_list.part templates/OrdererProfile_${ORDERER_PROFILE}.yaml > ${TMP_DIR}/OrdererProfile_1.yaml
+    PATTERN=%ORDERER_ORGS% templates/templater.awk ${TMP_DIR}/configtx_orgs_list.part ${TMP_DIR}/OrdererProfile_1.yaml > ${TMP_DIR}/OrdererProfile_2.yaml
+    PATTERN=%RAFT_CONSENTERS% templates/templater.awk ${TMP_DIR}/configtx_consenters_list.part ${TMP_DIR}/OrdererProfile_2.yaml > ${TMP_DIR}/OrdererProfile.yaml
 
-    envsubst < "templates/configtx-template-dynamic.yaml" > "crypto-config/configtx_1.yaml"
-    PATTERN=%ORGS_DEFINITIONS% templates/templater.awk crypto-config/configtx_org_definitions.part crypto-config/configtx_1.yaml > crypto-config/configtx.yaml
-    cat crypto-config/OrdererProfile.yaml >> crypto-config/configtx.yaml
+    envsubst < "templates/configtx-template-dynamic.yaml" > "${TMP_DIR}/configtx_1.yaml"
+    PATTERN=%ORGS_DEFINITIONS% templates/templater.awk ${TMP_DIR}/configtx_org_definitions.part ${TMP_DIR}/configtx_1.yaml > ${TMP_DIR}/configtx.yaml
+    cat ${TMP_DIR}/OrdererProfile.yaml >> ${TMP_DIR}/configtx.yaml
 
-    envsubst < "templates/cryptogen-orderer-template.yaml" > "crypto-config/cryptogen-orderer.yaml"
-    cat crypto-config/cryptogen-orderer-spec.yaml >>  "crypto-config/cryptogen-orderer.yaml"
+    envsubst < "templates/cryptogen-orderer-template.yaml" > "${TMP_DIR}/cryptogen-orderer.yaml"
+    cat ${TMP_DIR}/cryptogen-orderer-spec.yaml >>  "${TMP_DIR}/cryptogen-orderer.yaml"
 
 }
 
 function generateCryptoMaterialIfNotExists() {
-    if [ ! -f "crypto-config/ordererOrganizations/$ORDERER_DOMAIN/orderers/${ORDERER_NAME}.$ORDERER_DOMAIN/msp/admincerts/Admin@$ORDERER_DOMAIN-cert.pem" ]; then
-        echo "Crypto-config not exists. File does not exists: crypto-config/ordererOrganizations/$ORDERER_DOMAIN/orderers/${ORDERER_NAME}.$ORDERER_DOMAIN/msp/admincerts/Admin@$ORDERER_DOMAIN-cert.pem"
+    if [ ! -f "${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/orderers/${ORDERER_NAME}.$ORDERER_DOMAIN/msp/admincerts/Admin@$ORDERER_DOMAIN-cert.pem" ]; then
+        echo "Crypto-config not exists. File does not exists: ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/orderers/${ORDERER_NAME}.$ORDERER_DOMAIN/msp/admincerts/Admin@$ORDERER_DOMAIN-cert.pem"
         echo "Generating orderer MSP."
-        rm -rf crypto-config/ordererOrganizations/$ORDERER_DOMAIN
-        cryptogen generate --config=crypto-config/cryptogen-orderer.yaml
+        rm -rf ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN
+        cryptogen generate --config=${TMP_DIR}/cryptogen-orderer.yaml --output="${TMP_DIR}"
     else
         echo "Orderer MSP exists. Generation skipped".
     fi
@@ -86,7 +90,7 @@ function generateCryptoMaterialIfNotExists() {
 function writeCryptogenOrgConfig() {
     local ordererName=${1:?Orderer name is required}
     local renewFiles=${2}
-    local cryptogenFile=crypto-config/cryptogen-orderer-spec.yaml
+    local cryptogenFile=${TMP_DIR}/cryptogen-orderer-spec.yaml
     echo -e "\n\n\tWriting cryptogen hostname for: $ordererName, start new file:$renewFiles"
     if [[ ${renewFiles} ]]; then
         touch ${cryptogenFile} && truncate -s 0 ${cryptogenFile}
@@ -105,12 +109,12 @@ function writeConfigtxOrgConfig() {
     local consenterId=${5:-1}
     local renewFiles=${6}
 
-    local orgDefinitionsFile=${7:-crypto-config/configtx_org_definitions.part}
-    local orgsListFile=${8:-crypto-config/configtx_orgs_list.part}
-    local addressesListFile=${9:-crypto-config/configtx_addresses_list.part}
-    local consentersListFile=${10:-crypto-config/configtx_consenters_list.part}
+    local orgDefinitionsFile=${7:-${TMP_DIR}/configtx_org_definitions.part}
+    local orgsListFile=${8:-${TMP_DIR}/configtx_orgs_list.part}
+    local addressesListFile=${9:-${TMP_DIR}/configtx_addresses_list.part}
+    local consentersListFile=${10:-${TMP_DIR}/configtx_consenters_list.part}
 
-    mkdir -p crypto-config/configtx/
+    mkdir -p ${TMP_DIR}/configtx/
     if [[ ${renewFiles} ]]; then
         touch ${orgDefinitionsFile} && truncate -s 0 ${orgDefinitionsFile}
         touch ${orgsListFile} && truncate -s 0 ${orgsListFile}
@@ -124,7 +128,7 @@ function writeConfigtxOrgConfig() {
     - &${aliasName}
         Name: $ordererName
         ID: $ordererName.$ordererDomain
-        MSPDir: /etc/hyperledger/crypto-config/ordererOrganizations/${ordererDomain}/msp
+        MSPDir: ${TMP_DIR}/ordererOrganizations/${ordererDomain}/msp
         Policies:
             Readers:
                 Type: Signature
@@ -165,16 +169,16 @@ function writeConfigtxOrgConfig() {
                   ClientTLSCert: ordererOrganizations/${ordererDomain}/orderers/${ordererName}.${ordererDomain}/tls/server.crt
                   ServerTLSCert: ordererOrganizations/${ordererDomain}/orderers/${ordererName}.${ordererDomain}/tls/server.crt
 #                  MSPID: ${ordererName}.${ordererDomain}
-#                  Identity: ordererOrganizations/${ordererDomain}/orderers/${ordererName}.${ordererDomain}/msp/signcerts/${ordererName}.${ordererDomain}-cert.pem
+#                  Identity: ${TMP_DIR}/ordererOrganizations/${ordererDomain}/orderers/${ordererName}.${ordererDomain}/msp/signcerts/${ordererName}.${ordererDomain}-cert.pem
 #                  ConsenterId: $consenterId
     END
 }
 
 function generateGenesisBlockIfNotExists() {
-    if [ ! -f "crypto-config/configtx/$ORDERER_DOMAIN/genesis.pb" ]; then
+    if [ ! -f "${TMP_DIR}/configtx/$ORDERER_DOMAIN/genesis.pb" ]; then
         echo "Generating genesis configtx."
-        mkdir -p crypto-config/configtx/$ORDERER_DOMAIN
-        configtxgen -configPath crypto-config/ -outputBlock crypto-config/configtx/$ORDERER_DOMAIN/genesis.pb -profile ${ORDERER_PROFILE} -channelID ${SYSTEM_CHANNEL_ID}
+        mkdir -p ${TMP_DIR}/configtx/$ORDERER_DOMAIN
+        configtxgen -configPath ${TMP_DIR}/ -outputBlock ${TMP_DIR}/configtx/$ORDERER_DOMAIN/genesis.pb -profile ${ORDERER_PROFILE} -channelID ${SYSTEM_CHANNEL_ID}
     else
         echo "Genesis configtx exists. Generation skipped".
     fi
@@ -182,35 +186,35 @@ function generateGenesisBlockIfNotExists() {
 
 function copyWellKnownTLSCerts() {
     echo "Copying well-known tls certs to nginx "
-    mkdir -p crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/.well-known
-    cp crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/tlscacerts/tlsca.$ORDERER_DOMAIN-cert.pem crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/.well-known/msp-admin.pem 2>/dev/null
-    cp crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/tlscacerts/tlsca.$ORDERER_DOMAIN-cert.pem crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/.well-known/tlsca-cert.pem 2>/dev/null
+    mkdir -p ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/.well-known
+    cp ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/tlscacerts/tlsca.$ORDERER_DOMAIN-cert.pem ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/.well-known/msp-admin.pem 2>/dev/null
+    cp ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/tlscacerts/tlsca.$ORDERER_DOMAIN-cert.pem ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/.well-known/tlsca-cert.pem 2>/dev/null
 }
 
 function copyClientTLSCertForServingByWWW() {
     tlsCert="crypto-config/ordererOrganizations/$ORDERER_DOMAIN/orderers/${ORDERER_NAME}.$ORDERER_DOMAIN/tls/server.crt"
 
-    tlsNginxFolder=crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
+    tlsNginxFolder=${TMP_DIR}/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
     echo "Copying tls certs to nginx served folder $tlsCert"
     mkdir -p ${tlsNginxFolder}
     cp "${tlsCert}" "${tlsNginxFolder}"
 
     #deprecated
-    tlsNginxFolder=crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
+    tlsNginxFolder=${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
     echo "Copying tls certs to nginx served folder $tlsCert"
     mkdir -p ${tlsNginxFolder}
     cp "${tlsCert}" "${tlsNginxFolder}"
     ###########
 
     set -x
-    tlsNginxFolder=crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
+    tlsNginxFolder=${TMP_DIR}/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
     mkdir -p ${tlsNginxFolder}
     cp "${tlsCert}" "${tlsNginxFolder}"
 
-    mkdir -p crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp
-    cp -r crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/admincerts crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp 2>/dev/null
-    cp -r crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/cacerts crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp 2>/dev/null
-    cp -r crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/tlscacerts crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp 2>/dev/null
+    mkdir -p ${TMP_DIR}/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp
+    cp -r ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/admincerts crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp 2>/dev/null
+    cp -r ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/cacerts crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp 2>/dev/null
+    cp -r ${TMP_DIR}/ordererOrganizations/$ORDERER_DOMAIN/msp/tlscacerts crypto-config/node-certs/${ORDERER_NAME}.$ORDERER_DOMAIN/msp 2>/dev/null
     set +x
 
     if [[ -n "{ORG}" && -d "crypto-config/peerOrganizations/${ORG}.${DOMAIN}/msp/" ]]; then
@@ -218,11 +222,11 @@ function copyClientTLSCertForServingByWWW() {
 
         set -x
         #deprecated:
-        tlsNginxFolder=crypto-config/peerOrganizations/${ORG}.${DOMAIN}/msp/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
-        mkdir -p ${tlsNginxFolder}
-        cp "${tlsCert}" "${tlsNginxFolder}"
+#        tlsNginxFolder=crypto-config/peerOrganizations/${ORG}.${DOMAIN}/msp/${ORDERER_NAME}.$ORDERER_DOMAIN/tls
+#        mkdir -p ${tlsNginxFolder}
+#        cp "${tlsCert}" "${tlsNginxFolder}"
 
-        cp -r crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/* crypto-config/peerOrganizations/$ORG.$DOMAIN/msp 2>/dev/null
+#        cp -r crypto-config/ordererOrganizations/$ORDERER_DOMAIN/msp/* crypto-config/peerOrganizations/$ORG.$DOMAIN/msp 2>/dev/null
         ###########
         set +x
     fi
