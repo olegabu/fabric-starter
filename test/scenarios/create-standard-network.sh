c5392c6cdc9646738f6be1eb191e4285e6247ae8
skilpio <skilpio@gmail.com>
2021-11-30 20:18:53 +0300

    code clenup

--- test/scenarios/create-standard-network.sh
+++ test/scenarios/create-standard-network.sh
@@ -25,7 +25,7 @@ case "${DEPLOYMENT_TARGET}" in
     ;;
     vbox)   #dmachine
         pushd ../../ >/dev/null
-#        ./network-docker-machine-create.sh  ${@}
+#       ./network-docker-machine-create.sh  ${@}
         ./network-create.sh ${@}
 
         popd >/dev/null
