59ae2497458a2ba96625a69b73789723f5e37816
leonidle <>
2021-12-03 14:42:35 +0300

    node js dns chaincode cleanup

--- chaincode/2x/node/dns/fabric-shim/lib/utils/utils.js
+++
@@ -1,7 +0,0 @@
-module.exports.generateLoggingPrefix = (channelId, txId) => {
-    return `[${channelId}-${shortTxID(txId)}]`;
-};
-
-function shortTxID (txId) {
-    return txId.substring(0, 8);
-}
