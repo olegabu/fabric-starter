b4be4baebd52241c16d662c9d4a2ac42e2281fd8
skilpio <skilpio@gmail.com>
2021-12-06 17:54:17 +0300

    run commands in Fabric root dir

--- test/run-cli-peer.sh
+++ test/run-cli-peer.sh
@@ -3,9 +3,13 @@
 [ "${0#*-}" = "bash" ] && BASEDIR=$(dirname ${BASH_SOURCE[0]}) || BASEDIR=$(dirname $0) #extract script's dir
 
 compose_org=${1}
-#domain=${2}
 command=${@:2}
 
 libcommand='source container-scripts/lib/container-lib.sh 2>/dev/null'
+FABRIC_DIR=${FABRIC_DIR:-$BASEDIR/..}
+
+trap "popd >/dev/null" EXIT; #popd on process exit
+pushd "${FABRIC_DIR}" >/dev/null
 
 COMPOSE_PROJECT_NAME=${compose_org} docker-compose -f ${FABRIC_DIR}/docker-compose.yaml exec -T cli.peer bash -c "${libcommand}; ${command}"
+
