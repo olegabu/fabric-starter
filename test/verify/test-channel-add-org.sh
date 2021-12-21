3517d45ac99a04bd52f9633398dc1c7a415fdae3
skilpio <skilpio@gmail.com>
2021-12-21 18:47:12 +0300

    test network deployment refactored

--- test/verify/test-channel-add-org.sh
+++ test/verify/test-channel-add-org.sh
@@ -3,13 +3,13 @@
 [ "${0#*-}" = "bash" ] && BASEDIR=$(dirname ${BASH_SOURCE[0]}) || BASEDIR=$(dirname $0) #extract script's dir
 source "${BASEDIR}"/../libs/libs.sh
 
-channel=${1}
+channelName=${1}
 org=${2}
 orgAdd=${3:-${org}}
 
-printToLogAndToScreenBlue "\nVerifing if the [${orgAdd}] added to [${channel}]"
+printToLogAndToScreenBlue "\nVerifing if the [${orgAdd}] added to [${channelName}]"
 
 setCurrentActiveOrg ${org}
-result=$(runCLIPeer ${org} findOrgNameInChannelConfig ${channel} ${orgAdd})
+result=$(runCLIPeer ${org} findOrgNameInChannelConfig ${channelName} ${orgAdd})
 
-printResultAndSetExitCode "Organization [${orgAdd}] is in the channel [$channel]"
+printResultAndSetExitCode "Organization [${orgAdd}] is in the channel [$channelName]"
--- test/verify/test-join-channel.sh
+++ test/verify/test-join-channel.sh
@@ -3,13 +3,13 @@
 [ "${0#*-}" = "bash" ] && BASEDIR=$(dirname ${BASH_SOURCE[0]}) || BASEDIR=$(dirname $0) #extract script's dir
 source "${BASEDIR}"/../libs/libs.sh
 
-channel=${1}
+channelName=${1}
 org=${2}
 
-printToLogAndToScreenBlue "\nVerifing if the [${org}] has joined the [${channel}] channel..."
+printToLogAndToScreenBlue "\nVerifing if the [${org}] has joined the [${channelName}] channel..."
 
 setCurrentActiveOrg ${org}
-result=$(runCLIPeer ${org} peer channel list -o \$ORDERER_ADDRESS \$ORDERER_TLSCA_CERT_OPTS \| grep -E "^${channel}$")
+result=$(runCLIPeer ${org} peer channel list -o \$ORDERER_ADDRESS \$ORDERER_TLSCA_CERT_OPTS \| grep -E "^${channelName}$")
 
 setExitCode [ ! -z "${result}" ]
-printResultAndSetExitCode "The [${org}] org has joined the [${channel}] channel"
+printResultAndSetExitCode "The [${org}] org has joined the [${channelName}] channel"
