fc879a0197bec211e251495e3b218285aa5e6c5d
skilpio <skilpio@gmail.com>
2021-12-21 18:45:45 +0300

    test network deployment refactored

--- test/verify/test-chaincode-instantiated.sh
+++ test/verify/test-chaincode-instantiated.sh
@@ -3,15 +3,15 @@
 [ "${0#*-}" = "bash" ] && BASEDIR=$(dirname ${BASH_SOURCE[0]}) || BASEDIR=$(dirname $0) #extract script's dir
 source "${BASEDIR}"/../libs/libs.sh
 
-channel=${1}
+channelName=${1}
 org=${2}
 chaincode_init_name=${CHAINCODE_PREFIX:-reference}
-chaincode_name=${3:-${chaincode_init_name}_${channel}}
+chaincodeName=${3:-${chaincode_init_name}_${channelName}}
 
-printToLogAndToScreenBlue "\nVerifing if the test chaincode instantiated in channel [${channel}] channel by [${org}]"
+printToLogAndToScreenBlue "\nVerifing if the test chaincode instantiated in channel [${channelName}] channel by [${org}]"
 
 setCurrentActiveOrg ${org}
-result=$(runCLIPeer ${org} listChaincodesInstantiated ${channel} ${org} \| grep -E "^$chaincode_name")
+result=$(runCLIPeer ${org} listChaincodesInstantiated ${channelName} ${org} \| grep -E "^$chaincodeName")
 
 setExitCode [ ! -z "${result}" ]
-printResultAndSetExitCode "The test chaincode instantiated in [${channel}] channel by [${org}] org"
+printResultAndSetExitCode "The test chaincode instantiated in [${channelName}] channel by [${org}] org"
