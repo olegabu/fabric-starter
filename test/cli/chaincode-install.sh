71ae5451a5fac76546fc5f6111cc20b860fdf833
skilpio <skilpio@gmail.com>
2021-12-21 18:43:04 +0300

    test network deployment refactored

--- test/cli/chaincode-install.sh
+++ test/cli/chaincode-install.sh
@@ -3,10 +3,10 @@
 [ "${0#*-}" = "bash" ] && BASEDIR=$(dirname ${BASH_SOURCE[0]}) || BASEDIR=$(dirname $0) #extract script's dir
 source "${BASEDIR}"/../libs/libs.sh
 
-channel=${1}
+channelName=${1}
 org=${2}
 chaincode_init_name=${CHAINCODE_PREFIX:-reference}
-chaincode_name=${3:-"${chaincode_init_name}_${channel}"}
+chaincodeName=${3:-"${chaincode_init_name}_${channelName}"}
 lang=${4:-node}
 path=${5:-"${BASEDIR}/../resources/chaincode/${FABRIC_MAJOR_VERSION}x/${lang}/reference/."}
 
@@ -14,10 +14,10 @@ printToLogAndToScreenCyan "\nInstalling test chaincode in [${org}]"
 setCurrentActiveOrg ${org}
 
 printToLogAndToScreenCyan "\nCopying test chaincode to [${org}]"
-copyDirToContainer cli.peer0  ${org} ${DOMAIN:-example.com} "${path}" "${VERSIONED_CHAINCODE_PATH}/${lang}/${chaincode_name}"
+copyDirToContainer cli.peer0  ${org} ${DOMAIN:-example.com} "${path}" "${VERSIONED_CHAINCODE_PATH}/${lang}/${chaincodeName}"
 
-printToLogAndToScreenCyan "\nInstalling [$(getTestChaincodeName ${channel})] chaincode in [${org}]"
+printToLogAndToScreenCyan "\nInstalling [$(getTestChaincodeName ${channelName})] chaincode in [${org}]"
 result=$(runCLIPeer ${org} \
-  "./container-scripts/network/chaincode-install.sh '${chaincode_name}' 1.0 ${VERSIONED_CHAINCODE_PATH}/${lang}/${chaincode_name} ${lang}")
+  "./container-scripts/network/chaincode-install.sh '${chaincodeName}' 1.0 ${VERSIONED_CHAINCODE_PATH}/${lang}/${chaincodeName} ${lang}")
 
 printResultAndSetExitCode "Test chaincode installed in [${org}]"
