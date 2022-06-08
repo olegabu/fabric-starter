11f8c14abc59ae325548a82f469e7fb1405b3c87
skilpio <skilpio@gmail.com>
2022-06-08 19:54:20 +0300

    use LC_NUMERIC='en_US.UTF-8' for calcs

--- test/libs/lib-scenario.sh
+++ test/libs/lib-scenario.sh
@@ -26,15 +26,15 @@ function runTestScenario() {
     unset IFS
 
     initResultsTable
-    
+
     IFS=',' read -r -a INTERFACE_TYPES <<< "${interfaceTypes}"
-    
+
     for SCRIPT_FOLDER in "${INTERFACE_TYPES[@]}"; do
-        
+
         printYellowBox "Running ${SCRIPT_FOLDER} tests"
         addTableRowSeparator
         pushd ${TEST_ROOT_DIR}/${SCRIPT_FOLDER}/ >/dev/null
-        SCENARIO ${scenarioArgs} 
+        SCENARIO ${scenarioArgs}
         popd >/dev/null
     done
     printTestResultTable
@@ -42,7 +42,7 @@ function runTestScenario() {
 
 
 function initResultsTable() {
-    
+
     RESULTS+=("STEP|TEST NAME|RESULT|TIME ELAPSED (s)")
     START_TIME=$(LC_TIME="en_US.UTF-8" date)
 }
@@ -56,7 +56,7 @@ function addTableRowSeparator() {
 function getMaxTextLength() {
     local textLength=10
     local length
-    
+
     for line_n in "${RESULTS[@]}"; do
         local line="$(echo ${line_n} | cut -d '|' -f 2)"
         local length=$(expr length "${line}")
@@ -69,7 +69,7 @@ function getMaxTextLength() {
 
 
 function printLogStepHeader() {
-    
+
     printLog "${BRIGHT}${WHITE}---------------------------${NORMAL}"
     printLog "${BRIGHT}${WHITE}Step: $@${NORMAL}"
     printLog "${BRIGHT}${WHITE}---------------------------${NORMAL}"
@@ -78,21 +78,21 @@ function printLogStepHeader() {
 
 function printTestResultTable() {
     END_TIME=$(LC_TIME="en_US.UTF-8" date)
-    
+
     echo -e "\n\n"
-    
+
     local textlength=$(getMaxTextLength)
-    
+
     local l1=10
     local l2=$((textlength + 5))
     local l3=10
     local l4=10
-    
+
     local separator=$(printNSymbols '-' ${l1})"|"$(printNSymbols '-' ${l2})"|"$(printNSymbols '-' ${l3})"|"$(printNSymbols '-' ${l4})
-    
+
     local total_errors=0
     local tests_run=0
-    
+
     for result in "${RESULTS[@]}"; do
         if [ "${result}" = "-|-|-|-" ]; then result=${separator}; fi
         local testStep="$(echo ${result} | cut -d '|' -f 1)"
@@ -102,48 +102,48 @@ function printTestResultTable() {
 
         if [ "${exitCode}" = "0" ]; then
             tests_run=$(($tests_run + 1))
-            total_time=$(awk "BEGIN{print ${total_time} + ${elapsedTime}}")
+            total_time=$(LC_NUMERIC='en_US.UTF-8' awk "BEGIN{print ${total_time} + ${elapsedTime}}")
             exitCode="${BRIGHT}${GREEN}OK:  (${exitCode})${NORMAL}"
-            
+
             elif [[ ! ${exitCode} =~ ^[0-9]+$ ]]; then
             :
         else
             exitCode="${BRIGHT}${RED}ERR: (${exitCode})${NORMAL}"
             total_errors=$(($total_errors + 1))
             tests_run=$(($tests_run + 1))
-            total_time=$(awk "BEGIN{print ${total_time} + ${elapsedTime}}")
+            total_time=$(LC_NUMERIC='en_US.UTF-8' awk "BEGIN{print ${total_time} + ${elapsedTime}}")
         fi
         IFS='~'
         timing=$(printPaddingSpaces "${exitCode}" "${elapsedTime}" ${l3})
-        
+
         printf '%-'${l1}'s %-'${l2}'s %-'${l3}'s %-'${l4}'s\n' "${testStep}" "${testName}" "${exitCode}" "${timing}"
     done
     echo ${separator//|/ }
-    
+
     printYellow "Start time: ${WHITE}${START_TIME}${YELLOW}, End time: ${WHITE}${END_TIME}${YELLOW}"
-    
+
     if [ "${total_errors}" = 0 ]; then
         printYellow "Total tests run: ${WHITE}${tests_run}${YELLOW} \nTotal tests duration: ${WHITE}${total_time}${YELLOW} seconds \nTotal errors: ${WHITE}${total_errors}${YELLOW}"
     else
         printYellowRed "Total tests run: ${WHITE}${tests_run}${YELLOW} \nTotal tests duration: ${WHITE}${total_time}${YELLOW} seconds" "\nTotal errors: ${total_errors}"
     fi
-        
+
     printYellow "See debug log ${WHITE}${BRIHT}${FSTEST_LOG_FILE}${NORMAL}"
     echo
     IFS=
 }
 
 function runStep() {
-    
+
     local message=${1}
     shift
-    
+
     local COMMAND=$@
     local testRedErrOutput
 
     #Parse step command
     testRedErrOutput="false"
-    case "${COMMAND}" in 
+    case "${COMMAND}" in
     *VERIFY_NON_ZERO_EXIT_CODE:*)
         testRedErrOutput="true"
         ;;
@@ -159,39 +159,39 @@ function runStep() {
         sed -E -e 's/(VERIFY_NOT:)([^\n]*)/\1\2; verify_error=\$((\$run_error | \$verify_error | ! \$?)); echo \"\$(setColorOnError \${verify_error})\${BRIGHT}Non-zero exit code expected \${NORMAL}\"; /g' |\
         sed -E -e 's/(VERIFY_NON_ZERO_EXIT_CODE:)([^\n]*)/; verify_error=\$((! \$run_error | $verify_error)); run_error=0; echo \"\$(setColorOnError \${verify_error})\${BRIGHT}Non-zero exit code expected \${NORMAL}\"; /g' |\
         tr '\n' ' ' )
-    
+
     COMMAND=${COMMAND}'; [[ $verify_error = "0" ]]'
     COMMAND=${COMMAND//RUNTEST:[[:space:]]/" NO_RED_OUTPUT=${testRedErrOutput} ./"}
     COMMAND=${COMMAND//RUNTESTNOERRPRINT:[[:space:]]/" NO_RED_OUTPUT=true ./"}
     COMMAND=${COMMAND//VERIFY_NOT:[[:space:]]/" NO_RED_OUTPUT=true ${TEST_ROOT_DIR}/${VERIFY_SCRIPT_FOLDER}/"}
     COMMAND=${COMMAND//VERIFY:[[:space:]]/" ${TEST_ROOT_DIR}/${VERIFY_SCRIPT_FOLDER}/"}
     COMMAND=${COMMAND//RUN:[[:space:]]/;}
-    
+
     COMMAND=$(echo $COMMAND | sed -E -e 's/;([[:space:]]*)/;/g' -e 's/[;]+/;/g')
-    
+
     printWhite "\nStep $((++step))_${SCRIPT_FOLDER}: ${message}"
 
     printDbg $COMMAND
     printLogStepHeader ${step}_${SCRIPT_FOLDER}: ${message}
     printLog "$@"
-    
+
     #SET INDENTATION FOR /dev/stdout (1 tabulation symbol)
     exec 3>&1
     exec 1> >(paste /dev/null -)
-    
-    
+
+
     local startTime=$(LC_TIME="en_US.UTF-8" date +"%s.%3N")
     # Run command
     eval "${COMMAND}" 2>&1
     local exitCode=$?
     local stopTime=$(LC_TIME="en_US.UTF-8" date +"%s.%3N")
-    
-    local timeElapsed=$(awk "BEGIN{print ${stopTime} - ${startTime}}")
+
+    local timeElapsed=$(LC_NUMERIC='en_US.UTF-8' awk "BEGIN{print ${stopTime} - ${startTime}}")
     printDbg "Step ${step}_${SCRIPT_FOLDER}: exit code $exitCode"
-    
+
     #RESET INDENTATION FOR /dev/stdout
     exec 1>&3 3>&-
-    
+
     printExitCode "${exitCode}" "Step ${step}_${SCRIPT_FOLDER} exit code:"
     RESULTS+=("${step}_${SCRIPT_FOLDER}|${message}|${exitCode}|${timeElapsed}")
 }
