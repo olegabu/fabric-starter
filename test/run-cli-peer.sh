bebab19f613bd671169b0ca262a8a2e930dde4bf
skilpio <skilpio@gmail.com>
2022-06-08 14:29:34 +0300

    fix var names

--- test/run-cli-peer.sh
+++ test/run-cli-peer.sh
@@ -4,15 +4,15 @@
 
 main() {
 
-local compose_org=${1}
+local composeOrg=${1}
 local command=${@:2}
 
-libcommand='source container-scripts/lib/container-lib.sh 2>/dev/null'
+libCommand='source container-scripts/lib/container-lib.sh 2>/dev/null'
 FABRIC_DIR=${FABRIC_DIR:-$BASEDIR/..}
 
 pushd "${FABRIC_DIR}" >/dev/null
 
-COMPOSE_PROJECT_NAME=${compose_org} docker-compose -f ${FABRIC_DIR}/docker-compose.yaml exec -T cli.peer bash -c "${libcommand}; ${command}"
+COMPOSE_PROJECT_NAME=${composeOrg} docker-compose -f ${FABRIC_DIR}/docker-compose.yaml exec -T cli.peer bash -c "${libCommand}; ${command}"
 local exitCode=$?
 popd > /dev/null
 
