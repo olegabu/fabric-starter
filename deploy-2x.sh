105ff9cc6603091f74535561fd6bb5389b211fcb
leonidle <null>
2022-12-20 16:40:41 +0000

    stable

--- deploy-2x.sh
+++ deploy-2x.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export FABRIC_VERSION=${FABRIC_VERSION:-2.3}
-export FABRIC_STARTER_VERSION=${FABRIC_STARTER_VERSION:-latest}
+export FABRIC_STARTER_VERSION=${FABRIC_STARTER_VERSION:-stable}
 
 ./deploy.sh $@
